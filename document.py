8135524f681aa2a3f6328117f5841a29538081fd
Jerome White <jerome@thewex.net>
2016-07-19 11:54:11 +0400

    Sets all elements in the matrix to zero initially, then makes the diagonal one. Potentially faster than previous method of generating a zero lower-triangular and updating all values.

--- document.py
+++ document.py
@@ -15,12 +15,12 @@ class Corpus(list):
     def __str__(self):
         return ' '.join([ x.data for x in self ])
 
-    def similarity(self, segmenter, distance, parallel=1):
+    def similarity(self, segmenter, distance, parallel=1, orient=True):
         while True:
             words = segmenter.segment(str(self))
             try:
                 shape = [ len(words) ] * 2
-                dot = np.ones(shape, dtype=np.float16)
+                dot = np.zeros(shape, dtype=np.float16)
                 break
             except MemoryError:
                 cull = len(self) - (len(self) // 100)
@@ -31,8 +31,12 @@ class Corpus(list):
             following = i + 1
             for (j, c2) in enumerate(words[following:], following):
                 dot[(i, j)] = distance.distance(c1, c2)
-                
-        return np.transpose(np.fliplr(np.triu(dot)))
+        np.fill_diagonal(dot, 1)
+
+        if orient:
+            return np.transpose(np.fliplr(dot))
+        else:
+            return dot
         
     def dotplot(self, sim, fname):
         extent = [ 0, len(sim) ] * 2
