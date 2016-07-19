18b9a0819f8e08028089e2b4f5a34a5297b740aa
Jerome White <jerome@thewex.net>
2016-07-19 21:08:26 +0400

    Distance strings are instances of Python strings

--- distance.py
+++ distance.py
@@ -4,25 +4,22 @@ import numpy as np
 
 from nltk.metrics import distance
 
-class DistanceString:
-    def __init__(self, data):
-        self.data = data
-        
+class DistanceString(str):
     def __sub__(self, other):
         raise NotImplementedError
 
 class CharacterWiseDistance(DistanceString):
     def __sub__(self, other):
-        return np.mean([ x == y for (x, y) in zip(self.data, other.data) ])
+        return np.mean([ x == y for (x, y) in zip(self, other) ])
 
 class SequenceDistance(DistanceString):
     def __sub__(self, other):
-        seq = difflib.SequenceMatcher(None, self.data, other.data)
+        seq = difflib.SequenceMatcher(None, self, other)
         return seq.ratio()
 
 class Levenshtein(DistanceString):
     def __sub__(self, other):
-        ed = distance.edit_distance(self.data, other.data)
+        ed = distance.edit_distance(self, other)
         if ed != 0:
             ed **= -1
             
