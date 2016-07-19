2c0b22e4a79c4f2cdddde8cdc9345cfe65406c9b
Jerome White <jerome@thewex.net>
2016-07-19 15:35:14 +0400

    Test updated to use new underlying infrastructure.

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import segment
 import document
-import distance
+
+from segment import Slicer as segment_
+from distance import CharacterWiseDistance as distance_
 
 msg = 'text processing vs. speech processing'
 d = document.Document(None, None, msg)
 c = document.Corpus()
 c.append(d)
 
-s = segment.Slicer()
-d = distance.CharacterWiseDistance()
-dots = c.similarity(s, d)
+s = segment_(distance_)
+dots = c.similarity(s, parallel=4)
 c.dotplot(dots, 'aren.png')
