8b27d0e8dc0b35276dc28f54dbf6c5e25a0832cd
Jerome White <jerome@thewex.net>
2016-07-19 11:21:44 +0400

    Parallelization is now per DOC (within a WSJ document), rather than per WSJ document.

--- wsj.py
+++ wsj.py
@@ -1,49 +1,53 @@
 import re
+import logger
+import pickle
 import document
 import itertools
 
 import xml.etree.ElementTree as et
 
-from lib import logger
 from pathlib import Path
 from multiprocessing import Pool
 
-def wsj(fpath):
-    log = logger.getlogger()
-    log.info(str(fpath))
+def wsj(doc):
+    docno = doc.findall('DOCNO')
+    assert(len(docno) == 1)
+    docno = docno.pop().text.strip()
 
-    xml = re.sub('&', ' ', fpath.read_text())
-    # http://stackoverflow.com/a/23891895
-    combos = itertools.chain('<root>', xml, '</root>')
-    try:
-        root = et.fromstringlist(combos)
-    except et.ParseError as e:
-        log.error('{0}: line {1} col {2}'.format(str(fpath), *e.position))
-        return None
-    
-    for doc in root.findall('DOC'):
-        docno = doc.findall('DOCNO')
-        assert(len(docno) == 1)
-        docno = docno.pop().text.strip()
+    # log.info(docno)
 
-        text = []
-        for i in [ 'LP', 'TEXT' ]:
-            for j in doc.findall(i):
-                text.append(j.text)
-        text = ' '.join(' '.join(text).split()).lower()
+    text = []
+    for i in [ 'LP', 'TEXT' ]:
+        for j in doc.findall(i):
+            text.append(j.text)
+    text = ' '.join(' '.join(text).split()).lower()
             
-        yield document.Document(docno, fpath, text)
+    return (docno, text)
 
 log = logger.getlogger(True)
-log.info('Parse corpus')
+corpus = document.Corpus()
 
-with Pool(4, maxtasksperchild=1) as pool:
+with Pool() as pool:
     root = Path('/Users/jerome/Documents/Data/corpus/WSJ')
-    corpus = document.Corpus()
-    for i in pool.imap_unordered(wsj, root.glob('*/WSJ_*')):
-        if i:
-            corpus.append(i)
-
+    for fpath in root.glob('*/WSJ_*'):
+        log.info(str(fpath))
+        
+        xml = re.sub('&', ' ', fpath.read_text())
+        
+        # http://stackoverflow.com/a/23891895
+        combos = itertools.chain('<root>', xml, '</root>')
+        
+        try:
+            root = et.fromstringlist(combos)
+            for (i, j) in pool.imap_unordered(wsj, root.findall('DOC')):
+                d = document.Document(i, fpath, j)
+                corpus.append(d)
+        except et.ParseError as e:
+            log.error('{0}: line {1} col {2}'.format(str(fpath), *e.position))
+            
+    log.info('Corpus: {0}'.format(len(corpus)))
+    # pickle.dump(corpus, open('corpus_90-92.pkl', 'wb'))
+    
 log.info('similarity')
 s = corpus.similarity()
 log.info('plot')
