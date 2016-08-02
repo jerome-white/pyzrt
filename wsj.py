7620353ab8c394ae1163864a2852febbbc7a20ac
Jerome White <jerome@thewex.net>
2016-08-02 19:47:48 +0400

    Updates for new interface. Pickle file naming standardized.

--- wsj.py
+++ wsj.py
@@ -30,11 +30,12 @@ def wsj(doc):
 
 log = logger.getlogger(True)
 
-# corpus = pickle.load(open('corpus_90-92.pkl', 'rb'))
+corpus_pickle = 'corpus_1990-1992.pkl'
+# corpus = pickle.load(open(corpus_pickle, 'rb'))
 corpus = OrderedDict()
 
 with Pool() as pool:
-    root = Path('/Users/jerome/Documents/Data/corpus/WSJ')
+    root = Path('WSJ')
     for fpath in root.glob('*/WSJ_*'):
         log.info(str(fpath))
         
@@ -51,10 +52,10 @@ with Pool() as pool:
             log.error('{0}: line {1} col {2}'.format(str(fpath), *e.position))
             
     log.info('Corpus: {0}'.format(len(corpus)))
-    # pickle.dump(corpus, open('corpus_90-92.pkl', 'wb'))
+    pickle.dump(corpus, open(corpus_pickle, 'wb'))
 
-segmentation = segment.segment(corpus, 100)
-chunks = similarity.chunk(corpus, segmentation, distance.SequenceDistance)
-matrix = similarity.similarity(chunks)
+segmentation = segment.segment(corpus, 1000)
+chunks = similarity.chunk(corpus, segmentation)
+matrix = similarity.similarity(chunks, distance.sequence)
 dots = similarity.to_numpy(matrix)
 similarity.dotplot(dots, 'wsj.png')
