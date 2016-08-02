1c59f4f84434a08d64b8582c66806299b2d17288
Jerome White <jerome@thewex.net>
2016-08-02 19:46:47 +0400

    Updates for new segmentation and similarity interfaces.

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ corpus = {
     'd1': segment.Document(None, 'text processing vs. speech processing'),
 }
 
-segmentation = segment.segment(corpus, 1)
-chunks = similarity.chunk(corpus, segmentation, distance.CharacterWiseDistance)
-matrix = similarity.similarity(chunks)
+segmentation = segment.segment(corpus)
+chunks = similarity.chunk(corpus, segmentation)
+matrix = similarity.similarity(chunks, distance.characters)
 dots = similarity.to_numpy(matrix)
 similarity.dotplot(dots, 'test.png')
