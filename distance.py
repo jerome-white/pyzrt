2b60943e0592844849b34f94820a496173408949
Jerome White <jerome@thewex.net>
2016-08-02 19:45:33 +0400

    Distance now functional interface.

--- distance.py
+++ distance.py
@@ -4,23 +4,15 @@ import numpy as np
 
 from nltk.metrics import distance
 
-class DistanceString(str):
-    def __sub__(self, other):
-        raise NotImplementedError
+def characters(s1, s2):
+    return np.mean([ x == y for (x, y) in zip(s1, s2) ])
 
-class CharacterWiseDistance(DistanceString):
-    def __sub__(self, other):
-        return np.mean([ x == y for (x, y) in zip(self, other) ])
+def sequence(s1, s2):
+    return difflib.SequenceMatcher(None, s1, s2).ratio()
 
-class SequenceDistance(DistanceString):
-    def __sub__(self, other):
-        seq = difflib.SequenceMatcher(None, self, other)
-        return seq.ratio()
+def levenshtein(s1, s2):
+    ed = distance.edit_distance(self, other)
+    if ed != 0:
+        ed **= -1
 
-class Levenshtein(DistanceString):
-    def __sub__(self, other):
-        ed = distance.edit_distance(self, other)
-        if ed != 0:
-            ed **= -1
-            
-        return 1 - ed
+    return 1 - ed
