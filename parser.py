ec0a7c21f254ff95ff63b1cf416a33a014925d62
Jerome White <jerome@thewex.net>
2016-08-08 11:24:28 +0400

    Better customization for corpus types.

--- parser.py
+++ parser.py
@@ -13,30 +13,32 @@ class Parser():
     def f(self, doc):
         raise NotImplementedError
 
-    def paths(self):
-        raise NotImplementedError
-
-    def extract(self):
+    def extract(self, path):
         raise NotImplementedError
     
-    def parse(self):
+    def parse(self, top_level, glob_expression):
         log = logger.getlogger(True)
+        tld = Path(top_level)
         
         with mp.Pool() as pool:
-            for path in self.paths():
-                log.info(str(path))
+            for i in tld.glob(glob_expression):
+                log.info(str(i))
 
                 try:
-                    relevant = self.extract(path)
+                    relevant = self.extract(i)
                 except et.ParseError as e:
                     msg = '{0}: line {1} col {2}'
-                    log.error(msg.format(str(path), *e.position))
+                    log.error(msg.format(str(i), *e.position))
                     continue
                     
                 for (docno, data) in pool.imap_unordered(self.f, relevant):
-                    yield (docno, Document(path, data))
+                    document = Document(Path(i), data)
+                    yield (docno, document)
 
 class WSJParser(Parser):
+    def parse(self, top_level, glob_expression='*/WSJ_*'):
+        return super().parse(top_level, glob_expression)
+        
     def f(self, doc):
         docno = doc.findall('DOCNO')
         assert(len(docno) == 1)
@@ -49,15 +51,10 @@ class WSJParser(Parser):
         text = ' '.join(' '.join(text).split()).lower()
         
         return (docno, text)
-
-    def paths(self):
-        p = Path('WSJ')
-        
-        yield from p.glob('*/WSJ_*')
     
     def extract(self, path):
-        # http://stackoverflow.com/a/23891895            
         xml = re.sub('&', ' ', path.read_text())
+        # overcome pooly formed XML (http://stackoverflow.com/a/23891895)
         combos = itertools.chain('<root>', xml, '</root>')
         root = et.fromstringlist(combos)
         
