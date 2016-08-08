c6dd901ed643e4db566f7ef2ee32373c709205e1
Jerome White <jerome@thewex.net>
2016-08-08 11:25:45 +0400

    Updates for new corpus and similarity APIs. Support for command line options.

--- wsj.py
+++ wsj.py
@@ -2,35 +2,46 @@ import re
 import pickle
 import itertools
 
+from pathlib import Path
+from operator import itemgetter
+from argparse import ArgumentParser
+
 import logger
 import distance
 from parser import WSJParser
 from corpus import Corpus, Document
-from similarity import ComparisonPerCPU as Similarity
-
-import operator as op
-import multiprocessing as mp
-import xml.etree.ElementTree as et
-
-from pathlib import Path
-from collections import OrderedDict
+from similarity import RowPerCPU as Similarity
 
 log = logger.getlogger(True)
 
-corpus_pickle = 'corpus_1990-1992.pkl'
-# corpus = pickle.load(open(corpus_pickle, 'rb'))
+arguments = ArgumentParser()
+arguments.add_argument('--input-directory')
+arguments.add_argument('--output-directory')
+arguments.add_argument('--to-pickle')
+arguments.add_argument('--from-pickle')
+args = arguments.parse_args()
 
-parser = WSJParser()
-corpus = Corpus(parser.parse())
-pickle.dump(corpus, open(corpus_pickle, 'wb'))
+if args.from_pickle:
+    corpus.load(open(args.from_pickle), 'rb')
+else:
+    parser = WSJParser()
+    documents = parser.parse(args.input_directory)
+    corpus = Corpus(sorted(documents, key=itemgetter(0)))
+    
+if args.to_pickle:
+    pickle.dump(corpus, open(args.to_pickle, 'wb'))
 
 for i in [ 'documents', 'characters' ]:
     f = getattr(corpus, i)
     log.info('{0}: {1}'.format(i, f()))
+exit()
+output = Path(args.output_directory)
 
-s = Similarity()
-matrix = s.similarity(corpus.mend(corpus.fragment()))
-pickle.dump(matrix, open('wsj-matrix.pkl', 'wb'))
+fragments = FragmentedCorpus(corpus)
+matrix = Similarity(fragments.strings())
 
-dots = s.to_numpy(matrix)
-s.dotplot(dots, 'wsj.png')
+o = output.joinpath('wsj-matrix').with_suffix('.pkl')
+pickle.dump(matrix, o.open('wb'))
+
+o = output.joinpath('wsj').with_suffix('.png')
+matrix.dotplot(str(o))
