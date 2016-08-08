a1a6073f6f15ce9513ab05e28f8411c38a31ae8e
Jerome White <jerome@thewex.net>
2016-08-08 11:22:18 +0400

    Fragmented corpus is the go-between between the Corpus (now a dictionary) and the eventual similarity matrix. It has attributes that maintain the relationship, and methods allows index based lookup (where the similarity matrix index is assumed to be the fragment).

--- corpus.py
+++ corpus.py
@@ -1,51 +1,34 @@
-# import logger
-
 import numpy as np
 
 from collections import namedtuple, OrderedDict
 
 Document = namedtuple('Document', 'fpath, data')
-Fragment = namedtuple('Segment', 'docno, start, end')
-
-# class Fragment(list):
-#     def __init__(self, corpus):
-#         self.corpus = corpus
-        
-#     def __str__(self):
-#         s = []
-#         for (docno, start, end) in i:
-#             s = self.corpus[docno].data[start:end]
-#             string.append(s)
-            
-#         return ''.join(s)
+Fragment = namedtuple('Fragment', 'docno, start, end')
 
 class Corpus(OrderedDict):
-    def orange(self, start, stop, step, offset=None):
-        i = start
-        while i < stop:
-            if offset is not None:
-                j = i + offset
-                offset = None
-            else:
-                j = i + step
-            j = min(j, stop)
-            
-            yield (i, j)
-            i = j
+    def characters(self):
+        return sum([ len(data) for (_, data) in self.values() ])
 
-    def fragment(self, block_size=1):
+    def documents(self):
+        return len(self)
+
+class FragmentedCorpus(list):
+    def __init__(self, corpus, block_size=1):
+        self.corpus = corpus
+        
         remaining = None
         fragments = []
         length = 0
     
-        for (docno, doc) in self.items():
-            for (i, j) in self.orange(0, len(doc.data), block_size, remaining):
-                f = Fragment(docno, i, j)
+        for (docno, doc) in self.corpus.items():
+            for (i, j) in self.range_(0, len(doc.data), block_size, remaining):
+                f = Fragment(docno, *map(int, [ i, j ]))
                 fragments.append(f)
                 length += j - i
+                assert(length <= block_size)
                 
                 if length == block_size:
-                    yield fragments
+                    self.append(fragments)
                 
                     remaining = None
                     fragments = []
@@ -55,19 +38,31 @@ class Corpus(OrderedDict):
                 remaining = block_size - length
 
         if fragments:
-            yield fragments
+            self.append(fragments)
 
-    def mend(self, fragments=None):
-        for i in fragments:
+    def range_(self, start, stop, step, offset=None):
+        i = start
+        while i < stop:
+            if offset is None:
+                j = i + step
+            else:
+                j = i + offset
+                offset = None
+            j = min(j, stop)
+            
+            yield (i, j)
+            i = j
+            
+    def strings(self, start=0, end=None):
+        if end is None:
+            end = len(self)
+
+        for i in range(start, len(self)):
             string = []
-            for (docno, start, end) in i:
-                s = self[docno].data[start:end]
+            for (docno, start, end) in self[i]:
+                s = self.corpus[docno].data[start:end]
                 string.append(s)
             yield ''.join(string)
-            
-    def characters(self):
-        return sum([ len(data) for (_, data) in self.values() ])
 
-    def documents(self):
-        return len(self)
-            
+    def at(self, i):
+        return self.strings(i, i + 1)
