ade28d83071fb8c09817c066f444479ddc6c1918
Jerome White <jerome@thewex.net>
2016-08-11 06:23:14 -0400

    Fragments are now pickled.

--- wsj.py
+++ wsj.py
@@ -1,49 +1,58 @@
 import pickle
 import itertools
 
-from pathlib import Path
 from operator import itemgetter
 from argparse import ArgumentParser
 
 import logger
 import distance
+from output import Output
 from parser import WSJParser
 from corpus import Corpus, FragmentedCorpus
-from similarity import RowPerCPU as Similarity
+from similarity import ComparisonPerCPU as Similarity
 
 log = logger.getlogger(True)
 
 arguments = ArgumentParser()
 arguments.add_argument('--input-directory')
 arguments.add_argument('--output-directory')
-arguments.add_argument('--to-pickle')
-arguments.add_argument('--from-pickle')
+arguments.add_argument('--corpus-pickle')
+arguments.add_argument('--fragment-pickle')
 args = arguments.parse_args()
 
-if args.from_pickle:
-    with open(args.from_pickle, 'rb') as fp:
+output = Output(args.output_directory)
+
+#
+# Build the corpus
+#
+if args.corpus_pickle:
+    with open(args.corpus_pickle, 'rb') as fp:
         corpus = pickle.load(fp)
 else:
     parser = WSJParser()
     documents = parser.parse(args.input_directory)
     corpus = Corpus(sorted(documents, key=itemgetter(0)))
-    
-if args.to_pickle:
-    with open(args.to_pickle, 'wb') as fp:
-        pickle.dump(corpus, fp)
+    output.to_pickle('corpus', corpus)
 
 for i in [ 'documents', 'characters' ]:
     f = getattr(corpus, i)
     log.info('{0}: {1}'.format(i, f()))
-output = Path(args.output_directory)
 
-fragments = FragmentedCorpus(corpus)
+#
+# Build the fragments
+#
+if args.fragment_pickle:
+    with open(args.fragment_pickle, 'rb') as fp:
+        fragments = pickle.load(fp)
+else:
+    fragments = FragmentedCorpus(corpus)
+    output.to_pickle('fragments', fragments)
+
 log.info('Fragments {0}'.format(len(fragments)))
 
+#
+# Build the similarity matrix
+#
 matrix = Similarity(fragments)
-o = output.joinpath('wsj-matrix').with_suffix('.pkl')
-with o.open('wb') as fp:
-    pickle.dump(matrix, fp)
-
-o = output.joinpath('wsj').with_suffix('.png')
-matrix.dotplot(str(o))
+output.to_pickle('wsj-matrix', matrix)
+matrix.dotplot(str(output.fpath('wsj', 'png')))
