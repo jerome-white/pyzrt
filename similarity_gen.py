3f7127cf15e65255daed6504231419ea144454a8
Jerome White <jerome@thewex.net>
2016-08-14 12:02:49 +0400

    Completed partial functionality.

--- similarity_gen.py
+++ similarity_gen.py
@@ -1,21 +1,17 @@
-
-import sys
 import csv
-import corpus
 
-from pathlib import Path
-from archive import NamedSortedCorpus
 from argparse import ArgumentParser
+from multiprocessing import Manager
 
 arguments = ArgumentParser()
 arguments.add_argument('--corpus-directory')
-arguments.add_argument('--fragment-directory')
+arguments.add_argument('--fragment-file')
 args = arguments.parse_args()
 
 with open(arguments.corpus_directory) as fp:
     reader = csv.reader(fp)
     with Manager() as manager:
-        d = manager.dict()
-        d.update({ docno: data for (docno, data) in reader })
+        corpus = manager.dict()
+        corpus.update({ docno: data for (docno, data) in reader })
 
-        similarity.pairs
+        similarity.pairs(corpus, arguments.fragment_file)
