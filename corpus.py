e74a036cde1398734380cc6deba34df9f62691ca
Jerome White <jerome@thewex.net>
2016-08-14 10:07:15 +0400

    Produces rows for a fragmentation file rather than builds in-memory list.

--- corpus.py
+++ corpus.py
@@ -1,84 +1,51 @@
-import sys
-import csv
+from pathlib import Path
 
-import numpy as np
-
-from collections import namedtuple, OrderedDict
-
-Document = namedtuple('Document', 'fpath, data')
-Fragment = namedtuple('Fragment', 'docno, start, end')
-
-class Corpus(OrderedDict):
-    def characters(self):
-        return sum([ len(data) for (_, data) in self.values() ])
-
-    def documents(self):
-        return len(self)
-
-class FragmentedCorpus(list):
-    def __init__(self, corpus, block_size=1):
-        self.corpus = corpus
+class Notes:
+    def __init__(self, key=0):
+        self.key = key
+        self.length = 0
+        self.fragment = 0
+        self.remaining = None
         
-        remaining = None
-        fragments = []
-        length = 0
+def orange(self, start, stop, step, offset=None):
+    i = start
+    while i < stop:
+        if offset is None:
+            j = i + step
+        else:
+            j = i + offset
+            offset = None
+        j = min(j, stop)
+            
+        yield (i, j)
+        i = j
+
+def fragment(self, corpus_listing, block_size=1):
+    n = Notebook()
     
-        for (docno, doc) in self.corpus.items():
-            for (i, j) in self._range(0, len(doc.data), block_size, remaining):
-                f = Fragment(docno, *map(int, [ i, j ]))
-                fragments.append(f)
-                length += j - i
-                assert(length <= block_size)
+    for (docno, doc) in corpus_files:
+        for (i, j) in orange(0, len(doc.data), block_size, n.remaining):
+            yield (n.key, n.fragment, docno, i, j)
+            n.length += j - i
+            assert(n.length <= block_size)
                 
-                if length == block_size:
-                    self.append(fragments)
-                
-                    remaining = None
-                    fragments = []
-                    length = 0
-
-            if fragments:
-                remaining = block_size - length
-
-        if fragments:
-            self.append(fragments)
-
-    def _range(self, start, stop, step, offset=None):
-        i = start
-        while i < stop:
-            if offset is None:
-                j = i + step
+            if n.length == block_size:
+                n = Notebook(n.key + 1)
             else:
-                j = i + offset
-                offset = None
-            j = min(j, stop)
-            
-            yield (i, j)
-            i = j
+                n.fragment += 1
 
-    def at(self, i):
-        assert(0 <= i < len(self))
+        if n.fragment:
+            n.remaining = block_size - n.length
 
-        string = []
+    if n.fragment:
+        yield (n.key, n.fragment, docno, i, j)
+
+def to_string(fragment, corpus):
+    string = []
         
-        for (docno, start, end) in self[i]:
-            document = self.corpus[docno]
-            s = document.data[start:end]
-            string.append(s)
+    for (docno, start, end) in fragment:
+        document = corpus[docno]
+        s = document.data[start:end]
+        string.append(s)
             
-        return ''.join(string)
-        
-    def strings(self, start=0, end=None):
-        if end is None:
-            end = len(self)
-
-        yield from [ self.at(x) for x in range(start, len(self)) ]
-
-    def each(self):
-        for (i, pieces) in enumerate(self):
-            for (j, fragment) in enumerate(pieces):
-                yield (i, j, fragment.docno, fragment.start, fragment.end)
-                
-    def to_csv(self):
-        writer = csv.writer(sys.stdout)
-        writer.writerows(self.each())
+    return ''.join(string)
