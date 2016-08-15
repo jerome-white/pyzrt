b3a4c3bc23d0c7e1fc03a2e6ac697a030a3d606a
Jerome White <jerome@thewex.net>
2016-08-15 07:21:36 +0400

    Bug fixes to child process function; fixed file enumeration off-by-one error.

--- similarity.py
+++ similarity.py
@@ -1,54 +1,58 @@
 import sys
 import csv
 import logger
+import corpus
 
 import operator as op
-import multiprocessing as mp
 
-def pairs(corpus, fragment_file, distance=op.eq, parallel=None):
-    if parallel is not None:
-        parallel = min(mp.cpu_count(), max(parallel, 1))
+from collections import namedtuple
+from multiprocessing import Pool
 
-    with mp.Pool(parallel) as pool:
-        for _ in pool.imap_unordered(f, enum(corpus, fragment_file, distance)):
-            pass
+Args = namedtuple('Args', 'anchor, offset, corpus, fragment_file, distance')
+Fragment = namedtuple('Fragment', 'docno, start, end')
 
+def f(args):
+    s1 = None
+    frames = []
+    previous = args.anchor
+    peel = op.itemgetter(1)
+    writer = csv.writer(sys.stdout)
+    log = logger.getlogger()
+    
+    with open(args.fragment_file) as fp:
+        fp.seek(args.offset)
+        reader = csv.reader(fp)
+        for row in reader:
+            (current, key) = [ int(x) for x in row[:2] ]
+            fragment = Fragment(row[2], *map(int, row[3:]))
+            
+            if current != previous:
+                frames.sort(key=op.itemgetter(0))
+                string = corpus.to_string(map(peel, frames), args.corpus)
+                
+                if s1:
+                    d = args.distance(s1, string)
+                    writer.writerow([ args.anchor, previous, float(d) ])
+                else:
+                    s1 = string
+                    
+                frames = []
+            frames.append([ key, fragment ])
+            previous = current
+            
 def enum(corpus, fragment_file, distance):
     offset = 0
     previous = None
     
     with open(fragment_file) as fp:
         for line in fp:
-            current = line.split(',', 1)
+            (current, _) = line.split(',', 1)
             if previous is None or current != previous:
-                yield (current, offset, distance, fname)
+                yield Args(current, offset, corpus, fragment_file, distance)
             previous = current
-            offset += len(line)
-        
-def f(index, offset, distance, corpus, fragments):
-    s1 = None
-    frames = {}
-    previous = index
-    peel = itemgetter(1)
-    writer = csv.writer(sys.stdout)
-    
-    with open(fragments) as fp:
-        fp.seek(offset)
-        reader = csv.reader(fp)
-        for row in reader:
-            (current, key) = [ int(x) for x in row[:2] ]
-            fragment = Fragment(row[1:])
+            offset += len(line) + 1
             
-            if current != previous:
-                frames.sort(key=itemgetter(0))
-                string = mkstring(corpus, map(peel, frames))
-                
-                if s1:
-                    d = distance(s1, string)
-                    writer.writerow([ index, previous, d ])
-                else:
-                    s1 = string
-                    
-                frames = []                    
-            frames.append([ key, fragment ])
-            previous = current
+def pairs(corpus, fragment_file, distance=op.eq):
+    with Pool() as pool:
+        for _ in pool.imap_unordered(f, enum(corpus, fragment_file, distance)):
+            pass
