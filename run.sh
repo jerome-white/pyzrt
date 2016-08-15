e2511283ae6479921ae46e32000ace653235f44e
Jerome White <jerome@thewex.net>
2016-08-15 07:20:12 +0400

    All cases in workflow now accounted for.

--- run.sh
+++ run.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-#
-#
-#
-for i in `seq 1990 1992`; do
-    output=$SCRATCH/WSJ.fmt/$year
-    mkdir --parents $output
-    ls $SCRATCH/WSJ/$i/WSJ_* | \
-	python3 archive.py --output-directory $output --archive-type WSJ
+while getopts "t:c:f:" OPTION; do
+    case $OPTION in
+        t) task=$OPTARG ;;
+        c) corpus=$OPTARG ;;
+        f) fragments=$OPTARG ;;
+        *) exit 1 ;;
+    esac
 done
 
-#
-#
-#
+case $task in
+    archive)
+        for i in `seq 1990 1992`; do
+            output=$SCRATCH/WSJ.fmt/$year
+            mkdir --parents $output
+            ls $SCRATCH/WSJ/$i/WSJ_* | \
+	        python3 archive.py \
+                        --output-directory $output \
+                        --archive-type WSJ
+        done
+        ;;
+    fragment)
+        python3 fragment.py \
+                --corpus-directory $SCRATCH/WSJ.fmt/1990 > \
+                $SCRATCH/fragments.csv
+        ;;
+    similarity)
+        python3 similarity_gen.py \
+                --corpus-directory $corpus \
+                --fragment-file $SCRATCH/fragments.csv
+        ;;
+    *)
+        exit 1
+        ;;
+esac
