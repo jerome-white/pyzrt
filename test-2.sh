92f67dc183810d2cb77ff4510d0a4c0e21655ea1
Jerome White <jerome@thewex.net>
2016-08-16 10:01:38 +0400

    Added fragmentation test script; created new file naming scheme for tests.

---
+++ test-2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm --recursive --force test-docs
+mkdir --parents test-docs/{parsed,raw}
+for i in `seq 4`; do
+    for j in `seq 9`; do
+        echo $j
+    done > `mktemp --tmpdir=test-docs/raw`
+done
+
+ls test-docs/raw/* | \
+    python3 archive.py \
+            --archive-type test \
+            --output-directory test-docs/parsed
+
+python3 fragment.py \
+        --corpus-directory test-docs/parsed \
+        --block-size 4
+
+# python3 -u similarity-gen.py \
+#         --corpus-directory test-docs/parsed \
+#         --fragment-file test-docs/fragments.csv > test-docs/similarity.csv
+
+# python3 dot-gen.py --png test-docs/dot-plot.png < test-docs/similarity.csv
