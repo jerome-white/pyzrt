60e73793d44ecf94387b465b05762ed90546146c
Jerome White <jerome@thewex.net>
2016-09-01 19:02:52 +0400

    Removed managers. Each process has its own corpus, and the fragments are generated as needed from disk.

--- similarity.py
+++ similarity.py
@@ -13,73 +13,66 @@ from collections import namedtuple
 Args = namedtuple('Args', 'anchor, corpus, fragments, distance')
 Fragment = namedtuple('Fragment', 'docno, start, end')
 
+def frag(fragment_file, offset=0):
+    frames = []
+    previous = None
+    peel = lambda x: yield y.pop() for y in sorted(x, key=op.itemgetter(0))
+    
+    with open(fragment_file) as fp:
+        fp.seek(offset)
+        for row in csv.reader(fp):
+            (current, key) = map(int, row[:2])
+                
+            if previous is not None and previous != current:
+                yield (previous, peel(frames))
+                frames = []
+                    
+            fragment = Fragment(row[2], *map(int, row[3:]))
+            frames.append([ key, fragment ])
+            previous = current
+            
+        # since the last line of the file doesn't get included
+        if frames:
+            yield peel(frames)
+    
 def func(args):
     log = logger.getlogger()
     log.info(args.anchor)
+
+    corpus = {}
+    path = Path(args.corpus_directory)
+    for i in path.iterdir():
+        with i.open() as fp:
+            corpus[i.name] = fp.read()
+    log.debug('- corpus {0}'.format(len(corpus)))
     
     s1 = None
     writer = csv.writer(sys.stdout)
 
-    g = lambda x: to_string(args.corpus, x)
-    f = islice(args.fragments, args.anchor, None)
-    for (i, s2) in enumerate(map(g, f), args.anchor):
+    for (i, fragment) in frag(args.fragment_file, args.offset):
+        s2 = to_string(corpus, fragment)
         if s1 is None:
             s1 = s2
         else:
             d = args.distance(s1, s2)
             writer.writerow([ args.anchor, i, float(d) ])
             
-def enum(corpus, fragments, distance):
-    for (i, _) in enumerate(fragments):
-        yield Args(i, corpus, fragments, distance)
+def enum(corpus_directory, fragment_file, distance):
+    offset = 0
+    previous = None
+
+    with open(fragment_file) as fp:
+        for line in fp:
+            current = int(line.split(',', 1).pop(0))
+            if previous is None or previous != current:
+                yield Args(current, offset, corpus_directory, fragment_file,
+                           distance)
+                
+            previous = current
+            offset += len(line) + 1
         
 def pairs(corpus_directory, fragment_file, distance=op.eq):
-    log = logger.getlogger(True)
-    
-    with mp.Manager() as manager:
-        #
-        # Build the shared dictionary
-        #
-        log.info('+ corpus')
-        corpus = manager.dict()
-        
-        path = Path(corpus_directory)
-        for i in path.iterdir():
-            with i.open() as fp:
-                corpus[i.name] = fp.read()
-        log.info('- corpus {0}'.format(len(corpus)))
-
-        #
-        # Build the shared list
-        #
-        log.info('+ fragments')
-        fragments = manager.list()
-        
-        frames = []
-        previous = None
-        peel = lambda x: [ y.pop() for y in sorted(x, key=op.itemgetter(0)) ]
-        with open(fragment_file) as fp:
-            for row in csv.reader(fp):
-                (current, key) = map(int, row[:2])
-                
-                if previous is not None and previous != current:
-                    fragments.append(peel(frames))
-                    frames = []
-                    
-                fragment = Fragment(row[2], *map(int, row[3:]))
-                frames.append([ key, fragment ])
-                previous = current
-            # last line of the file doesn't get included
-            if frames:
-                fragments.append(peel(frames))
-        log.info('- fragments {0}'.format(len(fragments)))
-
-        #
-        # Spawn the children
-        #
-        log.info('+ similarity')
-        with mp.Pool() as pool:
-            iterable = enum(corpus, fragments, distance)
-            for _ in pool.imap_unordered(func, iterable):
-                pass
-        log.info('- similarity')
+    with mp.Pool() as pool:
+        iterable = enum(corpus, fragments, distance)
+        for _ in pool.imap_unordered(func, iterable):
+            pass
