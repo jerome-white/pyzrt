959eb9d31209e2732e4f815e8e57987b58610c33
Jerome White <jerome@thewex.net>
2016-09-05 09:31:43 -0400

    Option to specify memory map file.

--- church.py
+++ church.py
@@ -9,26 +9,27 @@ from corpus import from_disk
 from posting import Posting, Dotplot
 
 arguments = ArgumentParser()
+arguments.add_argument('--mmap')
 arguments.add_argument('--output-image')
 arguments.add_argument('--fragment-file')
 arguments.add_argument('--corpus-directory')
 arguments.add_argument('--threshold', default=np.inf, type=float)
-arguments.add_argument('--compression-ratio', default=1, type=float)
+arguments.add_argument('--compression', default=1, type=float)
 args = arguments.parse_args()
 
 corpus = dict(from_disk(args.corpus_directory))
 elements = sum([ len(x) for x in corpus.values() ])
-dp = Dotplot(elements, compression_ratio=args.compression_ratio)
+dp = Dotplot(elements, args.mmap, args.compression)
 
 with open(args.fragment_file) as fp:
     posting = Posting(fp, corpus)
 
-equality = lambda x: op.eq(*x)
+distinct = lambda x: op.ne(*x)
 
 for i in posting.posting.keys():
     if i.frequency() < args.threshold: # and i.isalpha():
         weight = posting.weight(i)
-        for (x, y) in filterfalse(equality, combinations(posting.each(i), 2)):
+        for (x, y) in filter(distinct, combinations(posting.each(i), 2)):
             dp.update(x, y, weight)
 
 dotplot(dp.dots, args.output_image)
