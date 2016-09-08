1fc2aee8ae4aa31689e49c801669257e64af15ae
Jerome White <jerome@thewex.net>
2016-09-08 07:33:15 -0400

    How the dot-array is stored is now a strategy. Mass is designed show how much a character contributes to the frequency.

--- posting.py
+++ posting.py
@@ -3,18 +3,19 @@ from collections import namedtuple, defaultdict
 
 import numpy as np
 
-import similarity
+import logger
 from corpus import to_string
+from similarity import Fragment, frag
 
 IndexedFragment = namedtuple('IndexedFragment',
-                             [ 'index' ] + list(similarity.Fragment._fields))
+                             [ 'index' ] + list(Fragment._fields))
 
 class Posting(defaultdict):
     def __init__(self, fragments, corpus=None):
         super().__init__(list)
 
         index = 0
-        for (_, i) in map(list, similarity.frag(fragments)):
+        for (_, i) in map(list, frag(fragments)):
             token = to_string(i, corpus)
             for (j, k) in enumerate(i, index):
                 self[token].append(IndexedFragment(j, *k))
@@ -22,6 +23,12 @@ class Posting(defaultdict):
     
     def frequency(self, token):
         return len(self[token]) if token in self else 0
+
+    def mass(self, token, relative=True):
+        counter = { x: sum([ len(z) for z in y ]) for (x, y) in self.items() }
+        c = counter[token]
+
+        return c / sum(counter.values()) if relative else c
 
     def weight(self, token):
         freq = self.frequency(token)
@@ -35,21 +42,29 @@ class Posting(defaultdict):
         yield from map(op.itemgetter(0), self[index])
 
 class Dotplot:
-    def __init__(self, total_elements, map_file=None, compression_ratio=1):
+    def __init__(self, total_elements, compression_ratio=1):
         assert(0 < compression_ratio <= 1)
         
         self.N = total_elements
         self.n = round(self.N * compression_ratio)
-        shape = [ self.n ] * 2
-        
-        if map_file:
-            self.dots = np.memmap(map_file, dtype=np.float16, shape=shape)
-        else:
-            self.dots = np.zeros(shape)
+        self.dots = self.mkdots(( self.n, ) * 2)
+
+    def mkdots(self, shape):
+        return np.zeros(shape)
 
     def cell(self, x):
-        return (x * self.n) / self.N
+        return (x * self.n) // self.N
     
     def update(self, row, col, value):
         coordinates = tuple(map(self.cell, [ row, col ]))
         self.dots[coordinates] += value
+
+class DistributedDotplot(Dotplot):
+    def __init__(self, total_elements, compression_ratio=1, map_file=None):
+        self.mmap = map_file
+        super().__init__(total_elements, compression_ratio)
+
+    def mkdots(self, shape):
+        log = logger.getlogger(True)
+        log.debug('{0} {1}'.format(self.mmap, shape))
+        return np.memmap(self.mmap, dtype=np.float16, mode='w+', shape=shape)
