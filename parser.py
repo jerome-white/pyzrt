437ea345c4a7bf58ad858580488ae22456c5ed78
Jerome White <jerome@thewex.net>
2016-09-18 17:07:05 +0400

    Moved corpus related material to own file.

--- parser.py
+++ parser.py
@@ -9,31 +9,12 @@ import xml.etree.ElementTree as et
 
 from pathlib import Path
 
-class Corpus(dict):
-    def __init__(self, path):
-        for i in path.iterdir():
-            with i.open() as fp:
-                self[i.name] = fp.read()
-
-class CorpusListing(list):
-    def __init__(self, directory):
-        path = Path(directory)
-        assert(path.is_dir())
-        super().__init__(self._sort(path.iterdir()))
-
-    def _sort(self, files):
-        raise NotImplementedError
-
-class NameSortedCorpus(CorpusListing):
-    def _sort(self, files):
-        return sorted(files)
-
 class Parser():
     def func(self, doc):
-        raise NotImplementedError
+        raise NotImplementedError()
 
     def extract(self, path):
-        raise NotImplementedError
+        raise NotImplementedError()
     
     def parse(self, file_list=sys.stdin):
         log = logger.getlogger(True)
