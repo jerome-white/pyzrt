d066690b701cdcad9f24c6078592461a35a23fb9
Jerome White <jerome@thewex.net>
2016-10-01 18:46:24 +0400

    Removed DistributedDotplot. Now a single Dotplot type that assumes memory mapping.

--- zrtlib/dotplot.py
+++ zrtlib/dotplot.py
@@ -7,36 +7,27 @@ from pathlib import Path
 from zrtlib import logger
 
 class Dotplot:
-    def __init__(self, total_elements, compression_ratio=1):
+    def __init__(self, total_elements, map_file, init_map=False,
+                 compression_ratio=1):
         assert(0 < compression_ratio <= 1)
         
         self.N = total_elements
         self.n = round(self.N * compression_ratio)
-        self.dots = self.mkdots(( self.n, ) * 2)
 
-    def mkdots(self, shape):
-        return np.zeros(shape)
+        kwargs = {
+            'filename': str(map_file.with_suffix('.' + str(self.n))),
+            'shape': ( self.n, ) * 2,
+            'mode': 'w+' if init_map else 'r+',
+            'dtype': np.float16,
+        }
+        self.matrix = np.memmap(**kwargs)
 
     def cell(self, x):
         return (x * self.n) // self.N
     
     def update(self, row, col, value):
         coordinates = tuple(map(self.cell, [ row, col ]))
-        self.dots[coordinates] += value
-
-class DistributedDotplot(Dotplot):
-    def __init__(self, total_elements, compression_ratio=1, map_file=None,
-                 init_map=False):
-        self.mmap = map_file
-        self.mode = 'write' if init_map else 'readwrite'
-
-        super().__init__(total_elements, compression_ratio)
-
-    def mkdots(self, shape):
-        suffix = '.' + str(self.n)
-        fname = str(self.mmap.with_suffix(suffix))
-
-        return np.memmap(fname, mode=self.mode, dtype=np.float16, shape=shape)
+        self.matrix[coordinates] += value
 
 def plot(dots, output):
     extent = [ 0, len(dots) ] * 2
