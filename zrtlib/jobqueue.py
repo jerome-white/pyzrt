bfa9ab57f1d1efec0ba495cef046977331b38602
Jerome White <jerome@thewex.net>
2016-10-05 02:19:28 -0400

    Context is required by Queue parent.

--- zrtlib/jobqueue.py
+++ zrtlib/jobqueue.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
+from multiprocessing import get_context
 from multiprocessing.queues import JoinableQueue
 
 Job = namedtuple('Job', 'key, indices, weight, dp')
 
 class JobQueue(JoinableQueue):
     def __init__(self, ledger):
-        super().__init__()
+        super().__init__(ctx=get_context())
         self.ledger = ledger
 
     def task_done(self, key):
