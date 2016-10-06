5d3e63f22e7474fa1af47bb5a0c82ae5f22f77f5
Jerome White <jerome@thewex.net>
2016-10-06 06:34:50 -0400

    Dotplot shape can be calculated from 1-dimension length.

--- src/plot.py
+++ src/plot.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from pathlib import Path
 from argparse import ArgumentParser
@@ -5,13 +6,15 @@ from argparse import ArgumentParser
 import zrtlib.dotplot as dp
 
 arguments = ArgumentParser()
-arguments.add_argument('--mmap')
-arguments.add_argument('--shape', type=int)
+arguments.add_argument('--mmap', type=Path)
 args = arguments.parse_args()
 
-# 20625
-shape = ( args.shape, ) * 2
-dots = np.memmap(args.mmap, dtype=np.float16, mode='r', shape=shape)
+dots = np.memmap(str(args.mmap), dtype=np.float16, mode='r')
+dim = math.sqrt(dots.size)
+assert(dim.is_integer())
 
-output = Path(args.mmap).with_suffix('.png')
+shape = (int(dim),) * 2
+dots = dots.reshape(shape)
+
+output = args.mmap.joinpath().with_suffix('.png')
 dp.plot(dots, str(output))
