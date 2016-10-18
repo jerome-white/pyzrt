0cc854777c430ce9b397c77b761f2f2d6747a8f6
Jerome White <jerome@thewex.net>
2016-10-18 16:43:12 +0400

    Can run tests on Pickle'd suffix trees.

--- test/suffix.py
+++ test/suffix.py
@@ -1,8 +1,14 @@
+import pickle
 import random
+from pathlib import Path
+from argparse import ArgumentParser
 from itertools import islice
 from collections import Counter
 
-from zrtlib.suffix import DebugSuffixTree as SuffixTree
+from zrtlib.suffix import SuffixTree
+# from zrtlib.suffix import DebugSuffixTree as SuffixTree
+
+ROWS_ = 70
 
 def randstr(length, constraint=1):
     assert(0 <= constraint <= 1)
@@ -14,29 +20,41 @@ def randstr(length, constraint=1):
 
     return ''.join(alphabet)
 
-c = Counter()
-s = SuffixTree()
+def fmtkey(key):
+    return "'" + key + "'"
 
-for i in range(4, 6):
-    for _ in range(10 ** 2):
-        key = randstr(i)
-        value = random.randrange(10)
-        s.add(key, value, 4)
-        c[key] += 1
+arguments = ArgumentParser()
+arguments.add_argument('--existing', type=Path)
+args = arguments.parse_args()
+
+c = Counter()
+if args.existing:
+    with args.existing.open('rb') as fp:
+        s = pickle.load(fp)
+else:
+    s = SuffixTree()
+
+    for i in range(4, 6):
+        for _ in range(10 ** 2):
+            key = randstr(i)
+            value = random.randrange(10)
+            s.add(key, value, 4)
+            c[key] += 1
 
 # for i in sorted(c):
 #     print(i, c[i])
 
-s.dump()
+# s.dump()
 
-# print('.' * 79)
-# for (i, j) in s.each():
-#     print(i, j)
-# print('.' * 79)
+print('.' * ROWS_)
+for (i, j) in s.each():
+    print(fmtkey(i), j)
+
+# print('.' * ROWS_)
 # for (i, j) in c.items():
 #     print(i, j, list(s.get(i)))
 
 for i in range(3, 7):
-    print('.' * 79)
+    print(i, '.' * ROWS_)
     for j in s.ngrams(i):
-        print(j, list(s.get(j)))
+        print(fmtkey(j), list(s.get(j)))
