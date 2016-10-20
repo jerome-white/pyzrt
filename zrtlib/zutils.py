72ab073f0164c008ec2eced5df85d0fe13614e2b
Jerome White <jerome@thewex.net>
2016-10-20 09:36:13 +0400

    Modification to itertools count to allow optional stopping.

--- zrtlib/zutils.py
+++ zrtlib/zutils.py
@@ -1,3 +1,4 @@
+import itertools
 import operator as op
 
 def pmap(f, iterable, start=0, stop=None):
@@ -6,3 +7,12 @@ def pmap(f, iterable, start=0, stop=None):
 
 def minval(iterable, item=0):
     return min(map(len, map(op.itemgetter(item), iterable)))
+
+
+def count(start=0, stop=None, inclusive=True):
+    rel = op.gt if inclusive else op.ge
+
+    for i in itertools.count(start):
+        if stop and rel(i, stop):
+            break
+        yield i
