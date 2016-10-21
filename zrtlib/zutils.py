e24c2d7411b6f2a062931900c89506dd93e2f90e
Jerome White <jerome@thewex.net>
2016-10-21 18:00:54 +0400

    Suffix functions moved to utils.

--- zrtlib/zutils.py
+++ zrtlib/zutils.py
@@ -1,13 +1,22 @@
 import itertools
 import operator as op
 
+def cut(word, pos=1):
+    return (word[0:pos], word[pos:])
+
+def substrings(word, length=None):
+    if length is None:
+        length = len(word) - 1
+
+    for i in range(len(word) - length + 1):
+        yield word[i:i+length]
+
 def pmap(f, iterable, start=0, stop=None):
     for (i, j) in enumerate(iterable):
         yield j if i < start or stop is not None and i >= stop else f(j)
 
 def minval(iterable, item=0):
     return min(map(len, map(op.itemgetter(item), iterable)))
-
 
 def count(start=0, stop=None, inclusive=True):
     rel = op.gt if inclusive else op.ge
