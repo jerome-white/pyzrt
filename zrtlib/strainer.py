aa404cac336997369d9067bfe479630062b67fd6
Jerome White <jerome@thewex.net>
2016-11-14 15:17:55 +0400

    Library for straining files now standalone. Uses decorator pattern.

---
+++ zrtlib/strainer.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as et
+
+class Strainer:
+    def __init__(self, strainer=None):
+        self.strainer = strainer
+        self.casing = False
+
+    def strain(self, document):
+        return document
+
+class AlphaNumericStrainer(Strainer):
+    def __init__(self, strainer, extended=False):
+        super().__init__()
+
+        self.casing = True
+        self.table = {}
+
+        for i in range(128):
+            c = chr(i)
+            self.table[i] = c if c.isalnum() else ' '
+
+        if extended:
+            self.table.update({ i: ' ' for i in range(128, 256) })
+
+    def strain(self, document):
+        document.text = document.text.translate(self.table)
+        return self.strainer.strain(document)
+
+class TRECStrainer(Strainer):
+    def strain(self, document):
+        top = et.Element('DOC')
+
+        docno = et.SubElement(top, 'DOCNO')
+        docno.text = document.docno
+
+        text = et.SubElement(top, 'TEXT')
+        text.text = document.text
+
+        document.text = et.tostring(top, encoding="unicode")
+        return self.strainer.strain(document)
