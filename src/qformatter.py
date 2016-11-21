aa9369ae8c34c60089ea7e8743c337489e317838
Jerome White <jerome@thewex.net>
2016-11-21 11:51:05 +0400

    Query docs class not restricted by list inheritance.

--- src/qformatter.py
+++ src/qformatter.py
@@ -3,24 +3,27 @@ import xml.etree.ElementTree as et
 from pathlib import Path
 from argparse import ArgumentParser
 
-class Query(list):
+class QueryDocs:
     def __init__(self, path):
         self.name = path.stem.zfill(3)
+        self.docs = []
 
-    def each(self):
-        for i in self:
-            yield et.tostring(i, encoding="unicode")
+    def __iter__(self):
+        yield from map(lambda x: et.tostring(x, encoding="unicode"), self.docs)
+
+    def __bool__(self):
+        return len(self.docs) > 0
 
     def add(self, query):
         doc = et.Element('DOC')
 
         docno = et.SubElement(doc, 'DOCNO')
-        docno.text = 'WSJQ00{0}-{1:04d}'.format(self.name, len(self))
+        docno.text = 'WSJQ00{0}-{1:04d}'.format(self.name, len(self.docs))
 
         text = et.SubElement(doc, 'TEXT')
         text.text = ' '.join(query)
 
-        self.append(doc)
+        self.docs.append(doc)
 
 arguments = ArgumentParser()
 arguments.add_argument('--input', type=Path)
@@ -29,7 +32,7 @@ arguments.add_argument('--include-topic', action='store_true')
 args = arguments.parse_args()
 
 for i in filter(lambda x: x.stem.isdigit(), args.input.iterdir()):
-    query = Query(i)
+    qdocs = QueryDocs(i)
     with i.open() as fp:
         q = []
         topic = args.include_topic
@@ -38,15 +41,15 @@ for i in filter(lambda x: x.stem.isdigit(), args.input.iterdir()):
                 q.append(line)
             else:
                 if topic:
-                    query.add(q)
+                    qdocs.add(q)
                 q = []
                 topic = True
 
         if q and topic:
-            query.add(q)
+            qdocs.add(q)
 
-    if len(query) > 0:
+    if qdocs:
         output = args.output.joinpath('WSJ_Q' + i.stem)
         with output.open('w') as fp:
-            for j in query.each():
+            for j in qdocs:
                 fp.write(j)
