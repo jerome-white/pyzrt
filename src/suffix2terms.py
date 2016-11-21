078556429e4ab46f9ec3bf3572a54f1d77948843
Jerome White <jerome@thewex.net>
2016-11-21 12:48:14 +0400

    Parallelizes document creation. Library for doing so deprecated.

--- src/suffix2terms.py
+++ src/suffix2terms.py
@@ -1,3 +1,5 @@
+import csv
+import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -6,16 +8,41 @@ from zrtlib.suffix import suffix_builder
 from zrtlib.tokenizer import TokenSet, unstream
 from zrtlib.pseudoterm import PseudoTermWriter
 
+def f(jobs, path):
+    log = logger.getlogger()
+    fieldnames = [ 'term', 'ngram', 'start', 'end' ]
+
+    while True:
+        (term, ngram, collection) = jobs.get()
+        log.info('{0} |{1}|'.format(term, ngram))
+
+        for token in collection:
+            for i in token:
+                row = dict(zip(fieldnames, [ term, ngram, i.start, i.end ]))
+                p = path.joinpath(i.docno.stem)
+
+                with p.open('a') as fp:
+                    writer = csv.DictWriter(fp, fieldnames=fieldnames)
+                    if fp.tell() == 0:
+                        writer.writeheader()
+                    writer.writerow(row)
+
+        jobs.task_done()
+
 arguments = ArgumentParser()
 arguments.add_argument('--suffix-tree', type=Path)
-arguments.add_argument('--basic-output', type=Path)
+arguments.add_argument('--output', type=Path)
+arguments.add_argument('--term-prefix', default='pt')
 args = arguments.parse_args()
 
-log = logger.getlogger()
+jobs = mp.JoinableQueue()
 
-log.info('suffix tree')
-suffix = suffix_builder(args.suffix_tree, unstream, TokenSet)
+with mp.Pool(initializer=f, initargs=(jobs, args.output)):
+    log.info('suffix tree')
+    suffix = suffix_builder(args.suffix_tree, unstream, TokenSet)
+    ngrams = len(str(len(suffix)))
 
-log.info('term writer')
-writer = PseudoTermWriter(s)
-writer.write(args.basic_output)
+    for (i, (ngram, collection)) in enumerate(suffix.each()):
+        term = args.term_prefix + str(i).zfill(ngrams)
+        jobs.put((term, ngram, collection))
+    jobs.join()
