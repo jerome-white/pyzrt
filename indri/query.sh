7410d8340a27da7d4adb62763fa83b0d5a768e87
Jerome White <jerome@thewex.net>
2016-12-09 18:57:45 +0000

    Handles lots of queries in a particular directory; and do evaluation if requested.

--- indri/query.sh
+++ indri/query.sh
@@ -3,59 +3,22 @@
 # http://lemur.sourceforge.net/indri/IndriRunQuery.html
 #
 
-mkquery() {
-    cat <<EOF
-<query>
-  <type>indri</type>
-  <number>$1</number>
-  <text>${@:2}</text>
-</query>
-EOF
-}
-
-mkqfile() {
-    n=0
-    include=$2
-    unset q
-
-    echo "<parameters>"
-    sed -e's/[^[:alnum:][:space:]]//g' $1 | { \
-	while read; do
-	    if [ -z "$REPLY" ]; then
-		if [ $include -eq 1 ]; then
-		    mkquery $n ${q[@]}
-		    (( n++ ))
-		fi
-		include=1
-		unset q
-	    else
-		q=( ${q[@]} $REPLY )
-	    fi
-	done
-	
-	if [ ${#q[@]} -gt 0 ] && [ $2 -eq 1 ]; then
-	    mkquery $n ${q[@]}
-	fi
-    }
-    echo "</parameters>"
-}
-
-index=$HOME/data/pyzrt/indri
 count=1000
-with_topic=0
-while getopts "i:q:c:th" OPTION; do
+while getopts "i:q:c:r:o:h" OPTION; do
     case $OPTION in
         i) index=$OPTARG ;;
 	q) queries=$OPTARG ;;
 	c) count=$OPTARG ;;
-	t) with_topic=1 ;;
+	r) qrels=$OPTARG ;;
+	o) output=$OPTARG ;;
 	h)
 	    cat <<EOF
 $0
-  -i location of index
-  -q location of queries
-  -c count
-  -t (include the topic description as a query)
+  -i index
+  -q queries
+  -c count (default $count)
+  -r relevance judgements (QRELS)
+  -o output
 EOF
 	    exit
 	    ;;
@@ -66,9 +29,10 @@ EOF
     esac
 done
 
-# mkqfile $queries $with_topic
-
-tmp=`mktemp`
-mkqfile $queries $with_topic > $tmp
-IndriRunQuery -count=$count -index=$index -trecFormat=true $tmp
-rm $tmp
+for i in $queries; do
+    results=$i.trec
+    IndriRunQuery -count=$count -index=$index -trecFormat=true $i > $results
+    if $qrels; then
+	trec_eval -q -c -M$count $qrels $results
+    fi
+done
