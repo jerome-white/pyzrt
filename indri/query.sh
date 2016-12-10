c5a23b350c83b2ab4dbcbd6892a889100f404863
Jerome White <jerome@thewex.net>
2016-12-10 11:19:51 -0500

    Only specify top-level directory.

--- indri/query.sh
+++ indri/query.sh
@@ -4,13 +4,11 @@
 #
 
 count=1000
-while getopts "i:q:c:r:o:h" OPTION; do
+while getopts "r:c:q:h" OPTION; do
     case $OPTION in
-        i) index=$OPTARG ;;
-	q) queries=$OPTARG ;;
+	r) root=$OPTARG ;;
 	c) count=$OPTARG ;;
-	r) qrels=$OPTARG ;;
-	o) output=$OPTARG ;;
+	q) qrels=$OPTARG ;;
 	h)
 	    cat <<EOF
 $0
@@ -18,7 +16,6 @@ $0
   -q queries
   -c count (default $count)
   -r relevance judgements (QRELS)
-  -o output
 EOF
 	    exit
 	    ;;
@@ -29,10 +26,17 @@ EOF
     esac
 done
 
-for i in $queries; do
-    results=$i.trec
-    IndriRunQuery -count=$count -index=$index -trecFormat=true $i > $results
-    if $qrels; then
-	trec_eval -q -c -M$count $qrels $results
+for i in $root/queries/*; do
+    j=`basename $i`
+    echo $j
+
+    a=$root/retrieved
+    mkdir --parents $a
+    IndriRunQuery -count=$count -index=$root/index -trecFormat=true $i > $a/$j
+
+    if [ $qrels ]; then
+	b=$root/evals
+	mkdir --parents $b
+	trec_eval -q -c -M$count $qrels/`basename $i` $a/$j > $b/$j
     fi
 done
