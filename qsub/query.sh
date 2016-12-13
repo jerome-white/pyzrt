278e6d509e6c9d9430be7e47c4cacc1a5735f4b7
Jerome White <jerome@thewex.net>
2016-12-13 11:08:35 -0500

    Works with QSUB.

--- qsub/query.sh
+++ qsub/query.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
+memory=60
+hours=3
 root=$WORK/wsj/2016_1128_014701
 count=1000
 
 for i in $root/indri/*; do
-    terms=`basename $i`
-
     for j in ua sa u1 un; do
-	#
-	# Build the queries
-	#
-	queries=$root/queries/$terms/$j
-	mkdir --parents $queries
+	qsub=`mktemp`
+	cat <<"EOF" > $qsub
+terms=`basename $i`
+queries=$root/queries/$terms/$j
+mkdir --parents $queries
 
-	find $root/pseudoterms/$terms -name 'WSJQ*' | \
-	    python $HOME/src/pyzrt/src/term2query.py \
-	    --output $queries \
-	    --model $j
+find $root/pseudoterms/$terms -name 'WSJQ*' | \
+    python $HOME/src/pyzrt/src/term2query.py \
+    --output $queries \
+    --model $j
 
-	#
-	# Run the queries
-	#
-	tmp=`mktemp`
-	for k in $queries/*; do
-	    IndriRunQuery \
-		-count=$count \
-		-index=$root/indri/$terms \
-		-trecFormat=true $k > \
-		$tmp
+tmp=`mktemp`
+for k in $queries/*; do
+    IndriRunQuery \
+	-count=$count \
+	-index=$root/indri/$terms \
+	-trecFormat=true $k > \
+	$tmp
 
-	    trec=$root/evals/$terms/$j/$i
-	    mkdir --parents `dirname $trec`
-	    trec_eval -q -c -M$count $WORK/qrels/`basename $i` $tmp > $trec
-	done
-	rm $tmp
+    topic=`cut --delimiter='-' --fields=1 <<< $(basename $k)`
+    trec=$root/evals/$terms/$j
+    mkdir --parents $trec
+
+    trec_eval -q -c -M$count $WORK/qrels/${topic:(-3)} $tmp > \
+        $trec/`basename $k`
+done
+rm $tmp
+EOF
+	qsub \
+	    -j oe \
+	    -l nodes=1:ppn=20,mem=${memory}GB,walltime=${hours}:00:00 \
+	    -m abe \
+	    -M jsw7@nyu.edu \
+	    -N query \
+	    -V \
+	    $qsub >> jobs
     done
 done
