834a6afaec2e791ac3ddc4a3f7e33a0cb30ed874
Jerome White <jerome@thewex.net>
2016-12-19 15:19:15 +0400

    Refined to only produce queries.

--- qsub/query.sh
+++ qsub/query.sh
@@ -29,57 +29,32 @@ done
 
 rm --force jobs
 for i in $root/indri/*; do
-    for j in ${models[@]}; do
-	echo $i $j
-
-	qsub=`mktemp`
-	cat <<EOF > $qsub
-i=$i
-j=$j
+    qsub=`mktemp`
+    echo $i $qsub
+    cat <<EOF > $qsub
 root=$root
-count=$count
-EOF
-	cat <<"EOF" >> $qsub
 terms=`basename $i`
-
-queries=$root/queries/$terms/$j
-rm --recursive --force $queries
-mkdir --parents $queries
-
-find $root/pseudoterms/$terms -name 'WSJQ*' | \
-    python $HOME/src/pyzrt/src/term2query.py \
-    --output $queries \
-    --model $j
-
-tmp=`mktemp`
-for k in $queries/*; do
-    IndriRunQuery \
-	-count=$count \
-	-index=$root/indri/$terms \
-	-trecFormat=true $k > \
-	$tmp
-
-    if [ `stat --format=%s $tmp` -gt 0 ]; then
-	topic=`cut --delimiter='-' --fields=1 <<< $(basename $k)`
-
-	trec=$root/evals/$terms/$j
-        rm --recursive --force $trec
-	mkdir --parents $trec
-
-	trec_eval -q -c -M$count $WORK/qrels/${topic:(-3)} $tmp > \
-	    $trec/`basename $k`
-    fi
-done
-rm $tmp
-echo DONE >&2
+for j in ${models[@]}; do
 EOF
-	qsub \
-	    -j oe \
-	    -l nodes=1:ppn=20,mem=${memory}GB,walltime=${hours}:00:00 \
-	    -m abe \
-	    -M jsw7@nyu.edu \
-	    -N query \
-	    -V \
-	    $qsub >> jobs
-    done
+    cat <<"EOF" >> $qsub
+    echo $terms $j
+    queries=$root/queries/$terms/$j
+    rm --recursive --force $queries
+    mkdir --parents $queries
+
+    find $root/pseudoterms/$terms -name 'WSJQ*' | \
+	python $HOME/src/pyzrt/src/term2query.py \
+	--output $queries \
+	--model $j
+done
+echo $terms DONE
+EOF
+    qsub \
+	-j oe \
+	-l nodes=1:ppn=20,mem=${memory}GB,walltime=${hours}:00:00 \
+	-m abe \
+	-M jsw7@nyu.edu \
+	-N query \
+	-V \
+	$qsub >> jobs
 done
