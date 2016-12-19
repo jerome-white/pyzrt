cb2e16c032f648b24d3ffd4a64b7a4ce6bff2bd6
Jerome White <jerome@thewex.net>
2016-12-19 18:00:24 +0400

    Only performs query/evaluation.

--- qsub/retrieve.sh
+++ qsub/retrieve.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-memory=60
-hours=4
-count=1000
-models=(
-    ua
-    sa
-    u1
-    un
-    uaw
-    saw
-)
-
 while getopts "r:h" OPTION; do
     case $OPTION in
 	r) root=$OPTARG ;;
@@ -27,59 +15,30 @@ EOF
     esac
 done
 
-rm --force jobs
-for i in $root/indri/*; do
-    for j in ${models[@]}; do
-	echo $i $j
+results=`mktemp`
+for term in $root/queries/*; do
+    terms=`basename $term`
 
-	qsub=`mktemp`
-	cat <<EOF > $qsub
-i=$i
-j=$j
-root=$root
-count=$count
-EOF
-	cat <<"EOF" >> $qsub
-terms=`basename $i`
-
-queries=$root/queries/$terms/$j
-rm --recursive --force $queries
-mkdir --parents $queries
-
-find $root/pseudoterms/$terms -name 'WSJQ*' | \
-    python $HOME/src/pyzrt/src/term2query.py \
-    --output $queries \
-    --model $j
-
-tmp=`mktemp`
-for k in $queries/*; do
-    IndriRunQuery \
-	-count=$count \
-	-index=$root/indri/$terms \
-	-trecFormat=true $k > \
-	$tmp
-
-    if [ `stat --format=%s $tmp` -gt 0 ]; then
-	topic=`cut --delimiter='-' --fields=1 <<< $(basename $k)`
-
-	trec=$root/evals/$terms/$j
-        rm --recursive --force $trec
+    for model in $term/*; do
+	trec=$root/evals/$terms/`basename $model`
+	rm --recursive --force $trec
 	mkdir --parents $trec
 
-	trec_eval -q -c -M$count $WORK/qrels/${topic:(-3)} $tmp > \
-	    $trec/`basename $k`
-    fi
-done
-rm $tmp
-echo DONE >&2
-EOF
-	qsub \
-	    -j oe \
-	    -l nodes=1:ppn=20,mem=${memory}GB,walltime=${hours}:00:00 \
-	    -m abe \
-	    -M jsw7@nyu.edu \
-	    -N query \
-	    -V \
-	    $qsub >> jobs
+	for query in $model/*; do
+	    echo $term $model $query
+
+	    IndriRunQuery \
+		-baseline=tfidf \
+		-count=$count \
+		-index=$root/indri/$terms \
+		-trecFormat=true \
+		$query > \
+		$results
+
+	    q=`basename $query`
+	    topic=`cut --delimiter='-' --fields=1 <<< $q`
+	    trec_eval -q -c -M$count $WORK/qrels/${topic:(-3)} $results > \
+		      $trec/$q
+	done
     done
 done
