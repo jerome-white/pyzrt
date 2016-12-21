b32d12f8bbff3383c1ee83153b82cc809b5ff2ec
Jerome White <jerome@thewex.net>
2016-12-21 18:14:29 +0400

    Uses Seaborn.

--- src/inter-compare.py
+++ src/inter-compare.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 from argparse import ArgumentParser
-from collections import defaultdict
 
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 from zrtlib import logger
 from zrtlib import zutils
+from zrtlib.indri import QueryDoc
 
 arguments = ArgumentParser()
-arguments.add_argument('--metric', action='append')
+arguments.add_argument('--metric')
 arguments.add_argument('--all', action='store_true') # "all" or runs only?
 arguments.add_argument('--zrt', type=Path)
 arguments.add_argument('--baseline', type=Path)
@@ -16,32 +18,27 @@ args = arguments.parse_args()
 
 log = logger.getlogger()
 
-metrics = {
+metric = {
     'map': 'MAP',
     'recip_rank': 'Reciprocal Rank',
-    }
+}[args.metric]
 
-for metric in args.metric:
-    labels = [ '\nSTD' ]
-    data = [ zutils.summary_stats(args.baseline, metric, args.all) ]
-    
-    for ngram in args.zrt.iterdir():
-        for model in ngram.iterdir():
-            lbl = '{0}\n{1}'.format(ngram.stem, model.stem.lower())
-            stats = zutils.summary_stats(model, metric, args.all)
-        
-            log.info(lbl.replace('\n', ' '))
-            
-            labels.append(lbl)
-            data.append(stats)
+stats = []
+for ngram in args.zrt.iterdir():
+    for model in ngram.iterdir():
+        for (topic, value) in zutils.get_stats(model, args.metric, args.all):
+            qid = QueryDoc.components(Path(topic))
+            stats.append({
+                'n-grams': ngram.stem,
+                'model': model.stem,
+                'topic': qid.topic,
+                metric: value,
+            })
+            log.debug(stats[-1].keys())
+data = pd.DataFrame(stats)
+fname = 'inter-{0}.png'.format(args.metric)
 
-    width = len(labels) * 0.75
-    plt.figure(figsize=(width, 12))
-    plt.boxplot(data, labels=labels, showfliers=False)
-
-    plt.ylim(ymin=0)
-    plt.grid(which='both', axis='y')
-    plt.ylabel(metrics[metric])
-
-    fname = 'inter-{0}.png'.format(metric)
-    plt.savefig(fname, bbox_inches='tight')
+plt.figure(figsize=(24, 6))
+#sns.set_context('tab')
+sns.barplot(x='n-grams', y=metric, hue='model', data=data)
+plt.savefig(fname, bbox_inches='tight')
