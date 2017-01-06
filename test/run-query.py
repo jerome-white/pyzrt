1938a3f33e9f0537334b2d2f0038cccf8ebd8b93
Jerome White <jerome@thewex.net>
2017-01-06 13:16:16 +0400

    Module to test creating and running query from within Python.

---
+++ test/run-query.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from argparse import ArgumentParser
+from tempfile import NamedTemporaryFile
+from collections import namedtuple
+
+import numpy as np
+
+# from zrtlib import logger
+from zrtlib.query import QueryBuilder
+from zrtlib.indri import QueryDoc, QueryExecutor
+from zrtlib.document import TermDocument # HiddenDocument
+
+arguments = ArgumentParser()
+arguments.add_argument('--index')
+arguments.add_argument('--query', type=Path)
+arguments.add_argument('--qrels', type=Path)
+args = arguments.parse_args()
+
+with QueryExecutor() as engine:
+    count = 1000
+    document = TermDocument(args.query)
+    
+    with NamedTemporaryFile(mode='w') as tmp:
+        print(QueryBuilder('ua', document), file=tmp)
+        result = engine.query(tmp.name, args.index, count)
+        assert(result.returncode == 0)
+
+    values = []
+    info = QueryDoc.components(args.query)
+    qrels = opts.qrels.joinpath(info.topic)
+    for line in engine.evaluate(str(qrels), count)
+        (metric, run, value) = line.strip().split()
+        if run.isdigit() and metric == opts.metric:
+            values.append(metric)
+    assert(values)
+    print(info.topic, np.mean(values))
