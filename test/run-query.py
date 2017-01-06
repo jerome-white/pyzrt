56e5d2455deca95edff5b749e5a3fcbaf782458c
Jerome White <jerome@thewex.net>
2017-01-06 11:05:07 -0500

    Debugged.

--- test/run-query.py
+++ test/run-query.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 import numpy as np
 
-# from zrtlib import logger
+from zrtlib import logger
 from zrtlib.query import QueryBuilder
 from zrtlib.indri import QueryDoc, QueryExecutor
 from zrtlib.document import TermDocument # HiddenDocument
@@ -14,24 +14,29 @@ arguments = ArgumentParser()
 arguments.add_argument('--index')
 arguments.add_argument('--query', type=Path)
 arguments.add_argument('--qrels', type=Path)
+arguments.add_argument('--indri')
+arguments.add_argument('--trec-eval')
+arguments.add_argument('--metric', default='map')
 args = arguments.parse_args()
 
-with QueryExecutor() as engine:
+log = logger.getlogger()
+
+with QueryExecutor(args.indri, args.trec_eval) as engine:
     count = 1000
-    document = TermDocument(args.query)
     
-    with NamedTemporaryFile(mode='w') as tmp:
-        print(QueryBuilder('ua', document), file=tmp)
+    with NamedTemporaryFile(mode='w', delete=False) as tmp:
+        log.debug(tmp.name)
+        print(QueryBuilder('ua', args.query), file=tmp, flush=True)
         result = engine.query(tmp.name, args.index, count)
         result.check_returncode()
 
     values = []
     info = QueryDoc.components(args.query)
-    qrels = opts.qrels.joinpath(info.topic)
+    qrels = args.qrels.joinpath(info.topic)
     for line in engine.evaluate(str(qrels), count):
         (metric, run, value) = line.strip().split()
-        if run.isdigit() and metric == opts.metric:
-            values.append(metric)
+        if run.isdigit() and metric == args.metric:
+            values.append(float(value))
     assert(values)
 
     print(info.topic, np.mean(values))
