b5130eb72205921b45356b367e04636f803c534f
Jerome White <jerome@thewex.net>
2017-01-08 07:13:40 -0500

    Reqires more memory.

--- qsub/reveal.sh
+++ qsub/reveal.sh
@@ -30,7 +30,7 @@ python3 $HOME/src/pyzrt/src/reveal.py \
 EOF
     qsub \
 	-j oe \
-	-l nodes=1:ppn=20,mem=60GB,walltime=6:00:00 \
+	-l nodes=1:ppn=20,mem=180GB,walltime=6:00:00 \
 	-m abe \
 	-M jsw7@nyu.edu \
 	-N reveal-$i \
