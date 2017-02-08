acec615a97b3f11dfa61921bb571578d39970c71
Jerome White <jerome@thewex.net>
2017-02-08 09:25:55 +0400

    Prevent creation of empty query files by generating the string before opening the file.

--- src/term2query.py
+++ src/term2query.py
@@ -5,26 +5,29 @@ from argparse import ArgumentParser
 
 from zrtlib import zutils
 from zrtlib import logger
+from zrtlib.indri import QueryDoc
 from zrtlib.query import QueryBuilder
 from zrtlib.document import TermDocument
 
 def func(args):
     (terms, model, output) = args
     
-    log = logger.getlogger()
-    log.info(terms.stem)
-
+    query = str(QueryBuilder(model, TermDocument(terms)))
     with output.joinpath(terms.stem).open('w') as fp:
-        q = QueryBuilder(model, TermDocument(terms))
-        fp.write(str(q))
-    
+        fp.write(query)
+
+    return terms
+
 arguments = ArgumentParser()
 arguments.add_argument('--model')
 arguments.add_argument('--input', type=Path)
 arguments.add_argument('--output', type=Path)
 args = arguments.parse_args()
 
+log = logger.getlogger()
+
 with mp.Pool() as pool:
     f = lambda x: (x, args.model.lower(), args.output)
-    for _ in pool.imap(func, map(f, zutils.walk(args.input))):
-        pass
+    for i in pool.imap(func, map(f, zutils.walk(args.input))):
+        qid = QueryDoc(i)
+        log.info(qid.topic)
