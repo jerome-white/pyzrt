830285744c6cd2f697a1f591eb472b61db3e50ec
Jerome White <jerome@thewex.net>
2017-02-20 08:55:03 +0400

    Uses Matplotlib's axhline directly.

--- src/overlap.py
+++ src/overlap.py
@@ -13,15 +13,6 @@ from zrtlib import logger
 from zrtlib import zutils
 from zrtlib.document import TermDocument
 
-def aquire(args, columns):
-    df = TermDocument(args.term_file).df
-    df = df[(df.start >= args.start) & (df.start <= args.end)]
-
-    for (i, row) in enumerate(df.itertuples(), 1):
-        for j in ('start', 'end'):
-            f = op.attrgetter(j)
-            yield dict(zip(columns, (f(row), i)))
-
 arguments = ArgumentParser()
 arguments.add_argument('--term-file', type=Path)
 arguments.add_argument('--start', type=int, default=0)
@@ -30,26 +21,28 @@ args = arguments.parse_args()
 
 log = logger.getlogger()
 
-columns = collections.OrderedDict([
-    ('x', 'location'),
-    ('y', 'term'),
-#    ('hue', '_')
-])
-# columns['hue'] = columns['x']
-
-df = pd.DataFrame(aquire(args, columns.values()))
-
-log.info('Plotting')
+document = TermDocument(args.term_file)
+df = document.df[['start', 'end']].reset_index().drop('index', axis=1)
+df = df[(df.start >= args.start) & (df.start <= args.end)]
+df /= df.max()
+df.index += 1
 
 sns.set_context('paper')
 sns.set(font_scale=1.7)
 sns.set_palette(sns.color_palette('Blues'))
 
-ax = sns.pointplot(data=df, ci=None, hue=columns['y'], **columns)
-ax.set(xticks=np.linspace(df.location.min(), df.location.max(), 5),
-       ylim=(0, None))
-ax.legend().remove()
+for row in df.itertuples():
+    plt.axhline(y=row.Index, xmin=row.start, xmax=row.end)
+
+plt.xlim(df.start.min(), df.end.max())
+plt.ylim(df.index[0] - 1, df.index[-1] + 1)
+# plt.yticks([ 'T' + str(x) for x in plt.yticks()[1:-1] ])
+
+# ax = sns.pointplot(data=df, ci=None, hue=columns['y'], **columns)
+# ax.set(xticks=np.linspace(df.location.min(), df.location.max(), 5),
+#        ylim=(0, None))
+# ax.legend().remove()
 
 fname = 'overlap-{0}.png'.format(args.term_file.stem)
 log.info(fname)
-ax.figure.savefig(fname, bbox_inches='tight')
+plt.savefig(fname, bbox_inches='tight')
