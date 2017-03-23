9bbd47a3de4559f80e9232908ce6f80ab2a2066a
Jerome White <jerome@thewex.net>
2017-03-23 15:29:25 +0400

    Clearing a file only called by one module; no need for global ability.

--- zrtlib/zutils.py
+++ zrtlib/zutils.py
@@ -4,11 +4,6 @@ import operator as op
 from pathlib import Path
 from functools import singledispatch
 
-def fclear(fp):
-    if fp.tell() > 0:
-        fp.seek(0)
-        fp.truncate()
-
 #
 # Read Indri stat output
 #
