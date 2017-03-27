ebbc5007e6668c226718e474eaec8c52e6c3d7db
Jerome White <jerome@thewex.net>
2017-03-27 10:43:42 +0400

    Plot flip (reveal) results using Seaborn.

--- vis/flips.py
+++ vis/flips.py
@@ -2,15 +2,38 @@ import csv
 from pathlib import Path
 from argparse import ArgumentParser
 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 arguments = ArgumentParser()
-arguments.add_argument('--metric', action='append')
+arguments.add_argument('--metric')
 arguments.add_argument('--input', type=Path)
 args = arguments.parse_args()
 
-keys = [ 'guess', 'term' ]
-keys.extend(args.metric)
+index = 'guess'
+usecols = [
+    index,
+    args.metric,
+]
 
-with args.input.open() as fp:
-    reader = csv.DictReader(fp)
-    for line in reader:
-        print(*[ line[x] for x in keys ])
+df = pd.read_csv(str(args.input),
+                 index_col=usecols.index(index),
+                 usecols=usecols)
+df = (df.
+      reindex(range(df.index.min(), df.index.max()), method='ffill').
+      reset_index().
+      assign(**dict(zip(('strategy', 'topic'), args.input.parts[-2:])))
+)
+
+plt.figure(figsize=(24, 6))
+# sns.set_context('paper')
+sns.set(font_scale=1.7)
+ax = sns.pointplot(x=index,
+                   y=args.metric,
+                   hue='strategy',
+                   data=df,
+                   markers='')
+ax.set(ylim=(0, None),
+       ylabel=args.metric)
+ax.figure.savefig('flips.png', bbox_inches='tight')
