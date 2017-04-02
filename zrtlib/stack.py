2eadc171f3ed923ce8478ba280884e8feeb19999
Jerome White <jerome@thewex.net>
2017-04-02 16:17:13 +0400

    Includes 'peel', which removes the current layer of the stack.

--- zrtlib/stack.py
+++ zrtlib/stack.py
@@ -2,16 +2,23 @@ class IterableStack:
     def __init__(self):
         self.stack = []
 
+    def __bool__:
+        return bool(self.stack)
+    
     def push(self, item):
         self.stack.append(list(item))
 
     def pop(self):
-        if not self.stack:
+        if not self:
             raise BufferError()
 
         last = self.stack[-1]
         item = last.pop(0)
         if not last:
-            self.stack.pop()
+            self.peel()
 
         return item
+
+    def peel(self):
+        if self:
+            self.stack.pop()
