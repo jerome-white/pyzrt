b7795c5b7449705e77ff3ba4f994a84fdc98effe
Jerome White <jerome@thewex.net>
2017-04-04 19:17:54 +0400

    Feedback handlers should take into account empty feedback. Weighted handlers should not assign zero weights to elements.

--- zrtlib/selector/feedback.py
+++ zrtlib/selector/feedback.py
@@ -3,12 +3,14 @@ import collections
 import numpy as np
 
 class FeedbackHandler(collections.deque):
-    def __init__(self, n=1):
-        super().__init__(maxlen=n)
+    def __init__(self, maxlen=1):
+        super().__init__(maxlen=maxlen)
 
     def __float__(self):
         raise NotImplementedError()
 
 class RecentWeighted(FeedbackHandler):
     def __float__(self):
-        np.average(self, weights=np.linspace(0, 1, len(self)))
+        weights = np.linspace(1, 2, len(self))
+
+        return np.average(self, weights=weights) if weights.size > 0 else 0.0
