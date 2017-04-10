213977db08be75b7da65b155cc31ed9551596be4
Jerome White <jerome@thewex.net>
2017-04-10 12:18:25 +0400

    Produces list of terms.

--- test/selector.py
+++ test/selector.py
@@ -5,7 +5,7 @@ import pandas as pd
 from zrtlib.document import TermDocument, HiddenDocument
 from zrtlib.selector.strategy import BlindHomogenous
 from zrtlib.selector.management import TermSelector
-from zrtlib.selector.technique import Random
+import zrtlib.selector.technique as tech
 
 path = Path('/',
             'Volumes',
@@ -19,16 +19,21 @@ path = Path('/',
             '07')
 docs = [ path.joinpath('WSJ900402-00' + str(x)) for x in range(17, 20) ]
 
-ts = TermSelector(BlindHomogenous(Random))
+ts = TermSelector(BlindHomogenous(tech.Entropy))
 for i in docs:
     ts.add(TermDocument(i))
 
 if __name__ == '__main__':
-    query = HiddenDocument(path.joinpath('WSJQ00298-0000'))
+    # query = HiddenDocument(path.joinpath('WSJQ00298-0000'))
+    # for i in ts:
+    #     assert(i in query.df[HiddenDocument.columns['visible']].values)
+    #     f = query.flip(i)
+    #     print(i, f)
+    s = set()
     for i in ts:
-        assert(i in query.df[HiddenDocument.columns['visible']].values)
-        f = query.flip(i)
-        print(i, f)
+        assert(i not in s)
+        print(i)
+        s.add(i)
 else:
     df = pd.concat(ts.documents.values(), copy=False)
     df.reset_index(drop=True, inplace=True)
