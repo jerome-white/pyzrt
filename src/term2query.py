4e7fa8f674478e1095fdb8ab0ee5e777f0f9feae
Jerome White <jerome@thewex.net>
2017-04-11 19:05:19 +0400

    Converting terms to queries can be done either in one shot or progressively. In the former, the entire term document is made into a single query; in the latter, a series of queries are built one term a time. Progress uses a hidden document structure to manage the per-term iteration.

--- src/term2query.py
+++ src/term2query.py
@@ -1,17 +1,33 @@
-import multiprocessing as mp
-
 from pathlib import Path
 from argparse import ArgumentParser
+from multiprocessing import Pool
 
 from zrtlib import zutils
 from zrtlib import logger
-from zrtlib.indri import QueryDoc
+from zrtlib.indri import IndriQuery
 from zrtlib.query import QueryBuilder
-from zrtlib.document import TermDocument
+from zrtlib.document import TermDocument, HiddenDocument
 
-def func(args):
+def progressive(args):
     (terms, model, output) = args
-    
+
+    log = logger.getlogger()
+    log.info(terms.stem)
+
+    indri = IndriQuery()
+    document = HiddenDocument(terms)
+
+    while document:
+        document.flip()
+        query = QueryBuilder(model, document)
+        indri.add(query.compose())
+
+    with output.joinpath(terms.stem).open('w') as fp:
+        fp.write(indri)
+
+def instantaneous(args):
+    (terms, model, output) = args
+
     query = str(QueryBuilder(model, TermDocument(terms)))
     with output.joinpath(terms.stem).open('w') as fp:
         fp.write(query)
@@ -22,12 +38,13 @@ arguments = ArgumentParser()
 arguments.add_argument('--model')
 arguments.add_argument('--input', type=Path)
 arguments.add_argument('--output', type=Path)
+arguments.add_argument('--progressive', action='store_true')
 args = arguments.parse_args()
 
 log = logger.getlogger()
 
-with mp.Pool() as pool:
-    f = lambda x: (x, args.model.lower(), args.output)
-    for i in pool.imap(func, map(f, zutils.walk(args.input))):
-        log.info(i.stem)        
-        qid = QueryDoc(i)
+with Pool() as pool:
+    func = progressive if args.progressive else instantaneous
+    mkargs = lambda x: (x, args.model.lower(), args.output)
+    for i in pool.imap(func, map(mkargs, zutils.walk(args.input))):
+        log.info(i.stem)
