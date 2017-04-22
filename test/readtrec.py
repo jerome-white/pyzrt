aee6f5e9a439b881342dc2b3b999878c376d601c
Jerome White <jerome@thewex.net>
2017-04-23 01:43:34 +0400

    Proper unit testing around trec_eval parsing.

--- test/readtrec.py
+++ test/readtrec.py
@@ -1,17 +1,49 @@
+import unittest
 from pathlib import Path
 
 from zrtlib import zutils
 
-path = Path('/',
-            'Volumes',
-            'Stick',
-            'wsj',
-            '2017_0118_020518',
-            'evals--progressive',
-            '04',
-            'ua',
-            'WSJQ00259-0000')
+class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        self.fp = Path('evals/progressive/04/ua/WSJQ00251-0000').open()
 
-with path.open() as fp:
-    for (i, _) in zutils.read_trec(fp, True):
-        print(i)
+    def tearDown(self):
+        self.fp.close()
+
+    def test_summaryNotReported(self):
+        for (i, _) in zutils.read_trec(self.fp):
+            self.assertFalse(i < 0)
+
+    def test_summaryReported(self):
+        negatives = []
+        for (i, _) in zutils.read_trec(self.fp, True):
+            if i < 0:
+                negatives.append(i)
+
+        self.assertTrue(len(negatives) == 1)
+
+    def test_summaryReportedOnce(self):
+        negatives = []
+        for (i, _) in zutils.read_trec(self.fp, True):
+            if i < 0:
+                negatives.append(i)
+        val = negatives.pop()
+        
+        self.assertEqual(val, -1)
+
+    def allComplete(self, summary):
+        keys = None
+        for (_, results) in zutils.read_trec(self.fp, summary):
+            if keys is None:
+                keys = set(results.keys())
+            else:
+                current = set(results.keys())
+                self.assertFalse(keys.isdisjoint(current))
+
+    def test_allCompleteWithSummary(self):
+        self.allComplete(True)
+
+    def test_allCompleteWithoutSummary(self):
+        self.allComplete(False)
+            
+unittest.main()
