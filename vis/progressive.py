73e823d77aef173c622e8160246fde68d84d9de6
Jerome White <jerome@thewex.net>
2017-04-23 08:15:24 +0400

    Adds plots individually using the Pandas plot interface.

--- vis/progressive.py
+++ vis/progressive.py
@@ -7,48 +7,46 @@ from multiprocessing import Pool
 
 import numpy as np
 import pandas as pd
-import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
 
 from zrtlib import logger
 from zrtlib import zutils
 from zrtlib.indri import QueryDoc
 
 def func(args):
-    (path, metric) = args
-
-    log = logger.getlogger()
-    log.info(path)
+    (path, opts) = args
 
     with path.open() as fp:
-        results = { x: y[metric] for (x, y) in zutils.read_trec(fp) }
+        df = pd.Series({x: y[opts.metric] for (x, y) in zutils.read_trec(fp)})
+        if opts.has_relevant and df.max() == 0:
+            return (path.stem, False)
 
-    return (pd.DataFrame.from_dict(results, orient='index').
-            reset_index().
-            assign(topic=int(QueryDoc.components(path).topic)).
-            rename(columns={ 'index': 'query', 0: 'metric' }).
-            apply(pd.to_numeric).
-            sort_values(by=[ 'topic', 'query' ]))
+    topic = QueryDoc.components(path).topic
+    output = opts.output.joinpath(topic).with_suffix('.png')
 
-def aquire(args):
-    with Pool() as pool:
-        iterable = map(lambda x: (x, args.metric), args.top_level.iterdir())
-        yield from pool.imap_unordered(func, iterable)
+    matplotlib.style.use('ggplot')
+    plt.clf()
+    df.plot(title=topic,
+            grid=True,
+            legend=False,
+            ylim=(0, 1))
+    plt.xlabel('Terms revealed')
+    plt.ylabel(opts.metric)
+    plt.savefig(str(output), bbox_inches='tight')
+
+    return (path.stem, True)
 
 arguments = ArgumentParser()
 arguments.add_argument('--metric')
+arguments.add_argument('--output', type=Path)
 arguments.add_argument('--top-level', type=Path)
+arguments.add_argument('--has-relevant', action='store_true')
 args = arguments.parse_args()
 
-df = pd.concat(aquire(args))
-# df.to_pickle('progressive.pkl')
+log = logger.getlogger(True)
 
-sns.set_context('poster')
-sns.set(font_scale=1.7)
-ax = sns.tsplot(time='query',
-                value='metric',
-                condition='topic',
-                data=df,
-                ci=30)
-# ax.set(xlim=(None, df['guess'].max()),
-#        ylim=(0, None))
-ax.figure.savefig('progressive.png', bbox_inches='tight')
+with Pool() as pool:
+    iterable = map(lambda x: (x, args), args.top_level.iterdir())
+    for (i, j) in pool.imap_unordered(func, iterable):
+        log.error(i) if not j else log.info(i)
