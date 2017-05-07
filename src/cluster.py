64240257f19ae00d3b632627685b1b0e3709520d
Jerome White <jerome@thewex.net>
2017-05-07 09:17:31 +0400

    Interface in line with cluster library.

--- src/cluster.py
+++ src/cluster.py
@@ -1,20 +1,23 @@
+import itertools
 from pathlib import Path
 from argparse import ArgumentParser
 from multiprocessing import Pool
 
 from zrtlib import cluster
 from zrtlib.indri import QueryDoc
+from zrtlib.cluster import KMeans
 
 arguments = ArgumentParser()
-arguments.add_argument('--plot', type=Path)
-arguments.add_argument('--save', type=Path)
+# arguments.add_argument('--plot', type=Path)
+# arguments.add_argument('--save', type=Path)
+# arguments.add_argument('--existing', type=Path)
+arguments.add_argument('--input', type=Path)
 arguments.add_argument('--output', type=Path)
-arguments.add_argument('--existing', type=Path)
-arguments.add_argument('--documents', type=Path)
 args = arguments.parse_args()
 
-documents = itertools.filterfalse(QueryDoc.isquery, args.documents.iterdir())
-cluster = KMeans(documents, n_cluster=50)
-cluster.write(args.output)
-if args.plot:
-    cluster.plot(args.plot)
+documents = itertools.filterfalse(QueryDoc.isquery, args.input.iterdir())
+cluster = KMeans(documents, n_cluster=50, n_jobs=-1)
+with args.output.open('w') as fp:
+    cluster.write(fp)
+# if args.plot:
+#     cluster.visualize(args.plot)
