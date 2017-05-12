214e64ac410ecc9f86bb33d8f5ea474be79ce9ab
Jerome White <jerome@thewex.net>
2017-05-12 08:22:44 -0400

    Which n-grams to work with controlled at the command line.

--- slurm/index.sh
+++ slurm/index.sh
@@ -1,89 +1,56 @@
 #!/bin/bash
 
-install() {
-    rm --recursive --force $1
-    mkdir --parents $1
-}
+#SBATCH --mem=60G
+#SBATCH --time=4:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jsw7@nyu.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=20
+#SBATCH --job-name=index
 
-while getopts "t:fih" OPTION; do
-    case $OPTION in
-	t) trees=$OPTARG ;;
-	f) mkterms=1 ;;
-	i) mkindex=1 ;;
-	h)
-	    cat<<EOF
-$0 -t trees (\$output in $ZR_HOME/qsub/suffix.sh)
-   -f Generate term files from a suffix tree
-   -i Generate Indri indexes from term files
-EOF
-	    exit
-	    ;;
-        *) exit 1 ;;
-    esac
-done
+#
+# Usage:
+#
+#  $> sbatch $0 n path/to/toplevel
+#
+# where n are the number of n-grams to work with and path/to/toplevel
+# is the path to the directory containing the trees ($output in
+# $ZR_HOME/qsub/suffix.sh)
+#
 
-if [ ! $mkterms ] && [ ! $mkindex ]; then
-    $0 -h
-    exit 1
-fi
+ngrams=`printf "%02d" ${1}`
 
-root=`dirname $trees`
+#
+# Convert the suffix trees to term files
+#
+pseudoterms=${2}/pseudoterms/$ngrams
+rm --recursive --force $pseudoterms
+mkdir --parents $pseudoterms
 
-rm --force jobs
-for i in $trees/*; do
-    term=`basename $i .csv`
-    echo $term
-
-    unset path
-    for j in pseudoterms documents indri; do
-	path=( ${path[@]} $root/$j/$term )
-    done
-
-    job=`mktemp`
-
-    #
-    # Convert the suffix trees to term files
-    #
-    if [ $mkterms ]; then
-	install ${path[0]}
-	cat <<EOF >> $job
 python3 $ZR_HOME/src/suffix2terms.py \
-  --suffix-tree $i \
-  --output ${path[0]}
-EOF
-    fi
-    
-    #
-    # Generate Indri indexes from term files
-    #
-    if [ $mkindex ]; then
-	for j in ${path[@]:1}; do
-	    install $j
-	done
-	cat <<EOF >> $job
-find ${path[0]} -name 'WSJ*' -not -name 'WSJQ*' | \
+  --suffix-tree ${2}/trees/$ngrams \
+  --output $pseudoterms
+
+#
+# Generate TREC formatted documents
+#
+documents=`mktemp --directory --tmpdir=$SLURM_JOBTMP`
+
+find $pseudoterms -name 'WSJ*' -not -name 'WSJQ*' | \
   python3 $ZR_HOME/src/parse.py \
-    --output ${path[1]} \
+    --output $documents \
     --parser pt \
     --strainer trec \
     --consolidate
 
+#
+# Generate Indri indexes from term files
+#
+index=${2}/index/$ngrams
+rm --recursive --force $index
+mkdir --parents $index
+
 IndriBuildIndex \
-  -corpus.path=${path[1]} \
+  -corpus.path=$documents \
   -corpus.class=trectext \
-  -index=${path[2]}
-EOF
-    fi
-
-    sbatch \
-        --mem=60G \
-        --time=4:00:00 \
-        --mail-type=ALL \
-	--mail-user=jsw7@nyu.edu \
-        --nodes=1 \
-        --cpus-per-task=20 \
-        --job-name=index \
-        $job >> jobs
-done
-
-# leave a blank line at the end
+  -index=$index
