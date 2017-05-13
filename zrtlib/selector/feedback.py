4ca36155b5c3631e1c66093817f296cb1c82739a
Jerome White <jerome@thewex.net>
2017-05-13 04:50:18 -0400

    Feedback prints last measurement when converted to string.

--- zrtlib/selector/feedback.py
+++ zrtlib/selector/feedback.py
@@ -20,6 +20,9 @@ class FeedbackHandler(collections.deque):
     def __float__(self):
         raise NotImplementedError()
 
+    def __str__(self):
+        return str(self[-1] if self else None)
+
 class RecentWeighted(FeedbackHandler):
     def __float__(self):
         return float(average(self))
