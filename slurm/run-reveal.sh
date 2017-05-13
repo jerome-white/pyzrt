8670ea97ee2f8aaf88e21e24a3bef5266fa560d8
Jerome White <jerome@thewex.net>
2017-05-13 04:42:52 -0400

    Sieve support.

--- slurm/run-reveal.sh
+++ slurm/run-reveal.sh
@@ -8,21 +8,30 @@ strategies=(
     relevance
     direct
     nearest
+    feedback
 )
 
-queries=( `seq 285 289` )
-# queries=( 285 )
+# queries=( `seq 285 289` )
+queries=( 259 )
 
 n=04
 root=$SCRATCH/zrt/wsj/2017_0118_020518
 for i in ${queries[@]}; do
     for j in ${strategies[@]}; do
-	echo -n "$i $j "
-	$ZR_HOME/qsub/reveal.sh \
-	    -q $root/pseudoterms/$n/WSJQ00${i}-0000 \
-	    -r $ZR_HOME/data/qrels.251-300.parts1-5.tar.gz \
-	    -s $j \
-	    -c 1000 \
-	    -x ndcg_cut.10
+	case $j in
+	    direct|nearest|feedback) sieve=( cluster term ) ;;
+	    *) sieve=( _ ) ;;
+	esac
+
+	for k in ${sieve[@]}; do
+	    echo -n "$i $j $k "
+	    $ZR_HOME/slurm/reveal.sh \
+		-q $root/pseudoterms/$n/WSJQ00${i}-0000 \
+		-r $ZR_HOME/data/qrels.251-300.parts1-5.tar.gz \
+		-s $j \
+		-c 1000 \
+		-x ndcg_cut.10 \
+		-v $k
+	done
     done
 done
