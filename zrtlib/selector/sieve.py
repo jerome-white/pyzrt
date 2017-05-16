998142e3e7d32e707c36fe1e5a057f21ec289e82
Jerome White <jerome@thewex.net>
2017-05-16 20:34:42 +0400

    Simplified document cluster logic.

--- zrtlib/selector/sieve.py
+++ zrtlib/selector/sieve.py
@@ -2,29 +2,31 @@ import pandas as pd
 
 class Sieve:
     def like(self, term, documents):
-        appearances = self.like_(term, documents)
+        appearances = self._like(term, documents)
         return documents[documents['document'].isin(appearances)]
 
-    def like_(self, term, documents):
+    def _like(self, term, documents):
         raise NotImplementedError()
 
 class TermSieve(Sieve):
-    def like_(self, term, documents):
+    def _like(self, term, documents):
         yield from documents[documents['term'] == term]['document']
 
 class ClusterSieve(TermSieve):
-    def __init__(self, clusters, sample=None):
+    def __init__(self, clusters):
         self.clusters = pd.read_csv(clusters, index_col=False)
-        self.sample = sample
 
-    def like_(self, term, documents):
-        relevant = super().like_(term, documents)
+    def _like(self, term, documents):
         column = 'value'
-        docs = self.clusters[(self.clusters['type'] == 'document') &
-                             (self.clusters[column].isin(relevant))]
 
-        for (_, group) in docs.groupby('cluster', sort=False):
-            df = group[column]
-            if self.sample is not None:
-                df = df.sample(self.sample)
-            yield df
+        # Documents containing this term...
+        appearances = super()._like(term, documents)
+
+        # ... clusters of those documents...
+        others = self.clusters[(self.clusters['type'] == 'document') &
+                               (self.clusters[column].isin(appearances))]
+
+        # ... documents in those clusters.
+        docs = self.clusters[self.clusters['cluster'].isin(others['cluster'])]
+
+        return docs[column]
