cdb252ba7aae379a8a940a1f6debfb5fb7b3eec8
Jerome White <jerome@thewex.net>
2017-09-25 11:21:58 +0400

    N-gram parser
    We're interested in system performance over word n-grams, instead of
    discovered n-grams. This file creates the basis for that (what comes
    out is ready to be converted to Indri-ready input).

---
+++ src/create/ngrams.py
@@ -0,0 +1,68 @@
+#
+# Parses a directory of files into n-grams. If max-gram's are
+# specified, then the set will be of variable length (between min-gram
+# and max-gram); it will be of constant size otherwise.
+#
+# Generally, files are assumed to have already been parsed;
+# specifically, that is, not in the TREC format.
+#
+
+import multiprocessing as mp
+from pathlib import Path
+from argparse import ArgumentParser
+
+from zrtlib import logger
+
+def func(incoming, output, n, m=None):
+    log = logger.getlogger()
+    collection = []
+
+    if m is None:
+        m = n
+    assert(0 < n <= m)
+    window = deque(maxlen=m)
+
+    while True:
+        document = incoming.get()
+        log.info(document.stem)
+
+        for i in (window, collection):
+            i.clear()
+
+        with document.open() as fp:
+            while True:
+                c = fp.read(1)
+                if c:
+                    window.append(c)
+                else:
+                    window.popleft()
+                    if len(window) < n:
+                        break
+
+                if len(window) >= n:
+                    ngram = ''.join(window).replace(' ', '_')
+                    collection.append(ngram)
+
+        if collection:
+            with output.joinpath(document.stem).open('w') as fp:
+                print(*collection, file=fp)
+        else:
+            log.warning(document.stem)
+
+        incoming.task_done()
+
+arguments = ArgumentParser()
+arguments.add_argument('--input', type=Path)
+arguments.add_argument('--output', type=Path)
+arguments.add_argument('--min-gram', type=int)
+arguments.add_argument('--max-gram', type=int, default=None)
+arguments.add_argument('--workers', type=int, default=mp.cpu_count())
+args = arguments.parse_args()
+
+outgoing = mp.JoinableQueue()
+initargs = (args.output, args.min_gram, args.max_gram)
+
+with mp.Pool(processes=args.workers, initializer=func, initargs=initargs):
+    for i in args.input.iterdir():
+        outgoing.put(i)
+    outgoing.join()
