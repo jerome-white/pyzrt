598a5ce360a859dd69d0f413b044f7afcb37e7e4
Jerome White <jerome@thewex.net>
2017-10-27 09:11:40 +0400

    Model specifications and partial functions.
    1. Ability to specify subsets of models
    2. Plots stored/accessed as partial functions.

--- src/vis/eval-compare.py
+++ src/vis/eval-compare.py
@@ -1,3 +1,4 @@
+import functools as ft
 import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
@@ -76,6 +77,7 @@ arguments.add_argument('--baseline', type=Path)
 arguments.add_argument('--min-ngrams', type=int, default=0)
 arguments.add_argument('--max-ngrams', type=float, default=np.inf)
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
+arguments.add_argument('--model', action='append')
 args = arguments.parse_args()
 
 log = logger.getlogger(True)
@@ -88,18 +90,23 @@ metric_label = {
     'map': 'Mean Average Precision',
     'recip_rank': 'Mean Reciprocal Rank',
 }[repr(args.metric)]
+
 if args.baseline:
     metric_label = 'Relative ' + metric_label
 
-(plotter, kwargs) = {
-    'bar': (sns.barplot, { 'errwidth': 0.1 }),
-    'point': (sns.pointplot, { 'ci': None }),
+plot = {
+    'bar': ft.partial(sns.barplot, errwidth=0.1),
+    'point': ft.partial(sns.pointplot, ci=None),
+    'strip': ft.partial(sns.stripplot),
 }[args.kind]
 
 #
 # Aquire the data
 #
 df = pd.concat(aquire(args))
+
+if args.model:
+    df = df[df['model'].isin(args.model)]
 
 if args.common:
     common = set()
@@ -111,6 +118,8 @@ if args.common:
             common = common.intersection(topics)
     log.debug(common)
     df = df[df['topic'].isin(common)]
+
+assert(not df.empty)
 
 topics = np.sort(df['topic'].unique())
 log.info('Topics ({0}): {1}'.format(len(topics), ','.join(map(str, topics))))
@@ -124,12 +133,11 @@ hues = df['model'].unique()
 sns.set_context('paper')
 #sns.set(font_scale=1.7)
 sns.set_style("whitegrid")
-ax = plotter(x='ngrams',
-             y=repr(args.metric),
-             hue='model',
-             hue_order=sorted(hues),
-             data=df,
-             **kwargs)
+ax = plot(x='ngrams',
+          y=repr(args.metric),
+          hue='model',
+          hue_order=sorted(hues),
+          data=df)
 ax.legend(ncol=round(len(hues) / 2), loc='upper center')
 ax.set(ylim=(0, args.y_max),
        ylabel=metric_label)
