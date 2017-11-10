1f0479446a00517fcd30f26fdee9aaaeada0fb81
Jerome White <jerome@thewex.net>
2017-11-10 11:27:09 +0400

    Addition of call to Word2Vec.
    Gone from skeleton that parsed sentences, to word2vec ready.

--- src/misc/term2vec.py
+++ src/misc/term2vec.py
@@ -1,17 +1,32 @@
 from pathlib import Path
 from argparse import ArgumentParser
 
+from gensim import Word2Vec
+
+from zrtlib import logger
 from zrtlib.terms import TermCollection
 
 def sentences(corpus):
+    log = logger.getlogger()
+
     for i in corpus.iterdir():
-        print(i)
+        log.debug(i.stem)
+
         terms = TermCollection(i)
         yield from map(str, terms.regions())
 
 arguments = ArgumentParser()
 arguments.add_argument('--corpus', type=Path)
+arguments.add_argument('--output', type=Path)
+arguments.add_argument('--workers', type=int)
 args = arguments.parse_args()
 
-for i in enumerate(sentences(args.corpus)):
-    print(*i)
+# for i in enumerate(sentences(args.corpus)):
+#     print(*i)
+
+log = logger.getlogger()
+
+log.info('BEGIN')
+model = Word2Vec(sentences(args.corpus), workers=args.workers)
+model.save(args.output)
+log.info('END')
