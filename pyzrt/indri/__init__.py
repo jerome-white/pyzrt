0b796446738102ba9e9e3ca3de6c1c73c0a5bc9b
Jerome White <jerome@thewex.net>
2017-11-19 17:10:42 +0400

    Imports brought to top level.

--- pyzrt/indri/__init__.py
+++
@@ -1,6 +0,0 @@
-from pyzrt.indri.doc import IndriQuery
-from pyzrt.indri.doc import QueryDocument
-
-from pyzrt.indri.sys import Search
-from pyzrt.indri.sys import TrecMetric
-from pyzrt.indri.sys import QueryRelevance
