c085e4d39ac88eaea39f89a4134de4961685a3a4
Jerome White <jerome@thewex.net>
2017-11-19 16:04:32 +0400

    Top level package.

---
+++ pyzrt/indri/__init__.py
@@ -0,0 +1,6 @@
+from pyzrt.indri.doc import IndriQuery
+from pyzrt.indri.doc import QueryDocument
+
+from pyzrt.indri.sys import Search
+from pyzrt.indri.sys import TrecMetric
+from pyzrt.indri.sys import QueryRelevance
