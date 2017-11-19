3bc3ba9de09a25e47135836cdafa4daccdd6e9e5
Jerome White <jerome@thewex.net>
2017-11-19 17:09:41 +0400

    Classes from indri and retrieval brought to top level.

--- pyzrt/__init__.py
+++ pyzrt/__init__.py
@@ -5,3 +5,11 @@ from pyzrt.core.collection import TermCollection
 
 from pyzrt.parsing.parser import Parser
 from pyzrt.parsing.strainer import Strainer
+
+from pyzrt.indri.doc import IndriQuery
+from pyzrt.indri.doc import TrecDocument
+from pyzrt.indri.sys import Search
+from pyzrt.indri.sys import TrecMetric
+from pyzrt.indri.sys import QueryRelevance
+
+from pyzrt.retrieval.query import Query
