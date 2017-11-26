59c8635b392ce97c1f40dc181c583630523005c0
Jerome White <jerome@thewex.net>
2017-11-26 10:15:53 +0400

    repr returns ngram.

--- pyzrt/core/term.py
+++ pyzrt/core/term.py
@@ -21,6 +21,9 @@ class Term(BasicTerm):
     def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return self.ngram
+
     @classmethod
     def _fromdict(cls, dictionary):
         d = { x: dictionary[x] for x in cls._fields }
