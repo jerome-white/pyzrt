5c7d5f0b01502ebe9b2858025be776157b8e944c
Jerome White <jerome@thewex.net>
2017-11-27 11:52:50 -0500

    Correct strainer specification.

--- slurm/corpus.sh
+++ slurm/corpus.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+basic=lower:symbol
+standard=space:alpha:$basic
 strainers=(
-    space:lower:alpha # "standard"
-    nospace:lower:alpha # no spaces
-    pause:nospace:lower:alpha # no spaces, pauses as periods
+    $standard # "standard"
+    clobber:$standard # no spaces
+    pause:clobber:$basic # no spaces, pauses as periods
 )
 documents=$HOME/etc/wsj/docs
 workers=20
@@ -11,25 +13,27 @@ workers=20
 for i in ${strainers[@]}; do
     output=$BEEGFS/corpus/`sed -e's/:/_/g' <<< $i`
 
-    jobs=`mktemp`
-    cat <<EOF > $jobs
+    job=`mktemp`
+    cat <<EOF > $job
 #!/bin/bash
 
 rm --force --recursive $output
 mkdir --parents $output
 
-python $ZR_HOME/src/create/parse.py \
+python $ZR_HOME/scripts/parse/parse.py \
         --parser wsj \
         --documents $documents \
         --workers $workers \
         --output $output \
         --strainer $i
 EOF
+
+    echo -n "$i $job "
     sbatch \
         --mem=60GB \
         --time=60 \
         --cpus-per-task=$workers \
         --nodes=1 \
         --job-name=$i \
-        $jobs
+        $job
 done
