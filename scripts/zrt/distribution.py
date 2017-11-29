b04043f00f9fde5a4208f8800a328fab964e9f75
Jerome White <jerome@thewex.net>
2017-11-29 15:08:04 +0400

    Switch to imap_unordered.
    Potential race condition in JobQueue.

--- scripts/zrt/distribution.py
+++ scripts/zrt/distribution.py
@@ -3,7 +3,6 @@ import collections as cl
 import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
-from multiprocessing import Pool
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -27,37 +26,34 @@ class Stats:
 
         return result
 
-def Collection(corpus_type):
+def Collection(corpus_type, document):
     return {
         'aren': pz.TermCollection.fromaren,
         'simulator': pz.TermCollection,
-    }[corpus_type]
+    }[corpus_type](document)
 
-def func(incoming, outgoing, creator):
+def func(args):
+    (document, creator) = args
+
     log = pz.util.get_logger()
+    log.info(document.stem)
 
-    collection = Collection(creator)
+    n = 0
+    names = set()
+    stats = Stats()
+    collection = Collection(creator, document)
 
-    while True:
-        document = incoming.get()
-        log.info(document.stem)
+    for region in collection.regions():
+        stats.terms.append(len(region))
+        for term in region:
+            names.add(str(term))
+            stats.durations.append(len(term))
+        n += 1
 
-        names = set()
-        stats = Stats()
-        tc = collection(document)
+    stats.regions.append(n)
+    stats.unique = len(names)
 
-        n = 0
-        for region in tc.regions():
-            stats.terms.append(len(region))
-            for term in region:
-                names.add(str(term))
-                stats.durations.append(len(term))
-            n += 1
-
-        stats.regions.append(n)
-        stats.unique = len(names)
-
-        outgoing.put(stats)
+    return stats
 
 arguments = ArgumentParser()
 arguments.add_argument('--terms', type=Path)
@@ -71,26 +67,26 @@ args = arguments.parse_args()
 
 assert(args.save or args.plot)
 
-log = pz.util.get_logger(True)
-
-incoming = mp.Queue()
-outgoing = mp.Queue()
-
-with Pool(args.workers, func, (outgoing, incoming, args.creator)) as pool:
-    jobs = pz.util.JobQueue(incoming, outgoing, args.terms.iterdir())
-    # stats = sum(jobs)
+with mp.Pool(args.workers) as pool:
     stats = Stats()
-    for i in jobs:
+    iterable = map(lambda x: (x, args.creator), args.terms.iterdir())
+    for i in pool.imap_unordered(func, iterable):
         stats += i
 
+log = pz.util.get_logger(True)
 for i in ('terms', 'regions', 'durations'):
     df = pd.Series(getattr(stats, i), name=i)
 
     if args.save:
         dat = args.save.joinpath(i).with_suffix('.csv')
+        log.info(dat)
+
         df.to_csv(dat, header=True, index_label='count')
 
     if args.plot:
+        img = args.plot.joinpath(i).with_suffix('.png')
+        log.info(img)
+
         df = df.value_counts().sort_index()
 
         if args.normalize:
@@ -103,6 +99,5 @@ for i in ('terms', 'regions', 'durations'):
 
         df.plot.line(grid=True, xlim=(args.x_min, None))
 
-        img = args.plot.joinpath(i).with_suffix('.png')
         plt.savefig(str(img), bbox_inches='tight')
         plt.clf()
