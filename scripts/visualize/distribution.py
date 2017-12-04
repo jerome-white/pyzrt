36b7d32a0e82ce4eab75c751aeab85148d4aee13
Jerome White <jerome@thewex.net>
2017-12-04 19:46:40 +0400

    Provide title and log output.

--- scripts/visualize/distribution.py
+++ scripts/visualize/distribution.py
@@ -10,8 +10,11 @@ import pyzrt as pz
 def func(args):
     (stats, measurement) = args
 
+    log = pz.util.get_logger()
+
     df = pd.read_csv(stats)
     if measurement in df.columns:
+        log.info(stats.stem)
         return df
 
 def aquire(measurement, directory):
@@ -24,6 +27,7 @@ arguments.add_argument('--terms', type=Path)
 arguments.add_argument('--output', type=Path)
 arguments.add_argument('--measurement')
 arguments.add_argument('--x-min', type=float, default=0)
+arguments.add_argument('--x-max', type=float)
 arguments.add_argument('--normalize', action='store_true')
 args = arguments.parse_args()
 
@@ -33,7 +37,11 @@ assert(not df.empty)
 df = df.pivot(index='count', columns='version', values=args.measurement)
 if args.normalize:
     df /= df.sum()
-df.plot.line(grid=True, xlim=(args.x_min, None))
+
+title = '../{0} - {1}'.format(args.terms.parts[-2:], args.measurement)
+df.plot.line(grid=True,
+             xlim=(args.x_min, args.x_max),
+             title=title)
 
 img = args.output.joinpath(args.measurement).with_suffix('.png')
 plt.savefig(str(img), bbox_inches='tight')
