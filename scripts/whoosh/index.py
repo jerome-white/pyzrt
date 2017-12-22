8a09c0ebcabaac7b7384eb6c2d69b72948c3f8ab
Jerome White <jerome@thewex.net>
2017-12-22 03:33:25 -0500

    Optimized analyser. Usage of write context.

--- scripts/whoosh/index.py
+++ scripts/whoosh/index.py
@@ -14,19 +14,23 @@ arguments.add_argument('--memory', type=int, default=128)
 arguments.add_argument('--workers', type=int, default=mp.cpu_count())
 args = arguments.parse_args()
 
+assert(not args.index.exists())
+
 log = pz.util.get_logger(True)
 
 log.info('|< BEGIN')
 with Pool(args.workers) as pool:
-    if args.index.exists():
-        ix = wndx.open_dir(args.index)
-    else:
-        ix = wndx.create_in(args.index, pz.WhooshEntry())
-    writer = ix.writer(procs=args.workers,
-                       limitmb=args.memory,
-                       multisegment=True)
-    for i in pool.imap_unordered(pz.WhooshEntr, pz.util.walk(args.corpus)):
-        log.debug(i.document)
-        writer.add_document(**i._asdict())
-    writer.commit()
+    args.index.mkdir()
+    ix = wndx.create_in(args.index, pz.WhooshSchema())
+    stem_ana = ix.schema['content'].format.analyzer
+    stem_ana.cachesize = -1
+    stem_ana.clear()
+
+    with ix.writer(procs=args.workers, limitmb=args.memory) as writer:
+        iterable = pz.util.walk(args.corpus)
+        for i in pool.imap_unordered(pz.WhooshEntry, iterable):
+            log.debug(i.document)
+            log.debug(i.content[:20])
+            writer.add_document(**i._asdict())
+        log.info('|+ COMMIT')
 log.info('|> COMPLETE')
