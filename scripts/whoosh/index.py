bedccb0e9fd37b317a3e367f36a0b3fe501f33e8
Jerome White <jerome@thewex.net>
2018-02-14 01:16:12 -0500

    Alternative merging strategy.
    Whoosh merging is slow. Part of the reason may be because of a
    suboptimal implementation of the Fibonacci sequence. This is an
    attempt to improve that.
    
    (Early testing shows it doesn't matter. Including here for future
    reference.)

--- scripts/whoosh/index.py
+++ scripts/whoosh/index.py
@@ -1,11 +1,57 @@
+import math
+import collections as cl
 import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
-from multiprocessing import Pool
 
 import whoosh.index as wndx
+from whoosh.reading import SegmentReader
 
 import pyzrt as pz
+
+class Fibonacci:
+    def __init__(self):
+        self.five = math.sqrt(5)
+        self.golden = (1 + self.five) / 2
+
+    def __call__(self, n):
+        return int((self.golden ** n - (-self.golden) ** -n) / self.five)
+
+def merge_small_(writer, segments):
+    log = pz.util.get_logger()
+    log.debug('segments {0}'.format(len(segments)))
+
+    segs = cl.defaultdict(list)
+    for i in segments:
+        size = i.doc_count_all()
+        segs[count].append(i)
+
+    docs = 0
+    pivot = False
+    merge = []
+    fib = Fibonacci()
+    for (i, s) in enumerate(sorted(segs.keys())):
+        docs += s
+        ptr = segs[s]
+        while ptr:
+            merge.append(ptr.pop())
+            if i > 3 and docs < fib(i + 5):
+                pivot = True
+                break
+        if pivot:
+            break
+    log.debug('merged {0}'.format(len(merge)))
+
+    for i in merge:
+        with SegmentReader(writer.storage, writer.schema, i) as reader:
+            writer.add_reader(reader)
+
+    remain = []
+    for i in segs.values():
+        remain.extend(i)
+    log.debug('remain {0}'.format(len(remain)))
+
+    return remain
 
 arguments = ArgumentParser()
 arguments.add_argument('--corpus', type=Path)
@@ -19,7 +65,7 @@ assert(not args.index.exists())
 log = pz.util.get_logger(True)
 
 log.info('|< BEGIN')
-with Pool(args.workers) as pool:
+with mp.Pool(args.workers) as pool:
     args.index.mkdir()
     ix = wndx.create_in(args.index, pz.WhooshSchema())
     # stem_ana = ix.schema['content'].format.analyzer
@@ -31,5 +77,6 @@ with Pool(args.workers) as pool:
         for i in pool.imap_unordered(pz.WhooshEntry, iterable):
             log.debug(i.document)
             writer.add_document(**i._asdict())
+        # writer.mergetype = merge_small_
         log.info('|+ COMMIT')
 log.info('|> COMPLETE')
