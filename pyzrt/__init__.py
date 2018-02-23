7765cdd80302f1403d5198405073cb8a9683cf56
Jerome White <jerome@thewex.net>
2018-02-23 10:44:07 +0400

    "Indri" becomes search.
    Files within the directory are no longer specialised to Indri related
    search. "Search" is the top-level class from which Indri descends.

--- pyzrt/__init__.py
+++ pyzrt/__init__.py
@@ -6,15 +6,15 @@ from pyzrt.core.collection import TermCollection
 from pyzrt.parsing.parser import Parser
 from pyzrt.parsing.strainer import Strainer
 
-from pyzrt.indri.doc import IndriQuery
-from pyzrt.indri.doc import TrecDocument
-from pyzrt.indri.sys import Search
-from pyzrt.indri.sys import TrecMetric
-from pyzrt.indri.sys import QueryRelevance
+from pyzrt.search.doc import IndriQuery
+from pyzrt.search.doc import TrecDocument
+from pyzrt.search.sys import Search
+from pyzrt.search.sys import TrecMetric
+from pyzrt.search.sys import QueryRelevance
 
-from pyzrt.indri.wh import WhooshSchema
-from pyzrt.indri.wh import WhooshEntry
-from pyzrt.indri.wh import WhooshSearch
-from pyzrt.indri.indri import IndriSearch
+from pyzrt.search.wh import WhooshSchema
+from pyzrt.search.wh import WhooshEntry
+from pyzrt.search.wh import WhooshSearch
+from pyzrt.search.indri import IndriSearch
 
 from pyzrt.retrieval.query import Query
