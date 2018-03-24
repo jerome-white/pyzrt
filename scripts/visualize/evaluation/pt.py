86727368b367de11968072876730bcfb1616b734
Jerome White <jerome@thewex.net>
2018-03-24 14:33:26 +0400

    Assume the melted data frame. Use difference as metric.

--- scripts/visualize/evaluation/pt.py
+++ scripts/visualize/evaluation/pt.py
@@ -1,4 +1,5 @@
 import sys
+import operator as op
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -16,26 +17,27 @@ arguments.add_argument('--metric', action='append')
 arguments.add_argument('--min-relevant', type=int, default=0)
 args = arguments.parse_args()
 
-metric_label = {
-    'map': 'MAP',
-    'recip_rank': 'Mean Reciprocal Rank',
-    'ndcg': 'NDCG',
-}
-metrics = [ repr(pz.TrecMetric(x)) for x in args.metric ]
+# metric_label = {
+#     'map': 'MAP',
+#     'recip_rank': 'Mean Reciprocal Rank',
+#     'ndcg': 'NDCG',
+# }
 
 df = pd.read_csv(sys.stdin)
-df = (df[df['num_rel'] >= args.min_relevant]
-      .melt(id_vars=['model', 'ngrams'],
-            value_vars=metrics,
-            var_name='metric',
-            value_name='value')
-      .sort_values(by=['metric', 'model', 'ngrams']))
+df = df[df['num_rel'] >= args.min_relevant]
+
+if df.columns.contains('value_baseline'):
+    normalization = op.sub
+    original = 'value_original'
+    df = df.rename(columns={'value': original})
+    df = df.assign(value=normalization(df[original], df['value_baseline']))
+df.to_csv('b', index=False)
 
 # sns.set_context('paper')
 #sns.set(font_scale=1.7)
 sns.set_style('whitegrid')
 
 g = sns.FacetGrid(df, row='metric', col='model')
-g.map(sns.pointplot, 'ngrams', 'value')
+g.map(sns.pointplot, 'ngrams', 'value', order=sorted(df['ngrams'].unique()))
 
 g.savefig(str(args.output), bbox_inches='tight')
