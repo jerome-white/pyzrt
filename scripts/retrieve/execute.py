87d3db0fb0c1904a533b28d9f68ae502cc2bfee3
Jerome White <jerome@thewex.net>
2018-03-26 06:27:03 -0400

    Simplified handling of execution model.
    Although this is likely unnecessary as most query interaction now
    happens using the tools directly.

--- scripts/retrieve/execute.py
+++ scripts/retrieve/execute.py
@@ -1,75 +1,37 @@
+import sys
 import csv
-import collections as cl
-import multiprocessing as mp
 from pathlib import Path
 from argparse import ArgumentParser
 
 import pyzrt as pz
 
-Info = cl.namedtuple('Info', 'model, query, ngrams')
-
-def func(queue, index, feedback, indri):
-    log = pz.util.get_logger()
-
-    metrics = map(pz.TrecMetric, feedback)
-
-    while True:
-        (query, output, qrels, info) = queue.get()
-        log.info('{0}'.format(query.name))
-
-        relevance = pz.QueryRelevance(qrels)
-        search = pz.IndriSearch(index, relevance, indri)
-        writer = None
-
-        with output.open('w') as fp:
-            for i in search.do(query, metrics):
-                entry = info._asdict()
-                assert(not any([ x in i.results for x in entry ]))
-                entry.update(**i.results)
-
-                if writer is None:
-                    writer = csv.DictWriter(fp, fieldnames=entry.keys())
-                    writer.writeheader()
-                writer.writerow(entry)
-
-        if not output.stat().st_size:
-            log.warning('{0} x'.format(query.name))
-            output.unlink()
-
-        queue.task_done()
-
 arguments = ArgumentParser()
+arguments.add_argument('--model')
 arguments.add_argument('--indri', type=Path)
 arguments.add_argument('--index', type=Path)
 arguments.add_argument('--qrels', type=Path)
-arguments.add_argument('--queries', type=Path)
-arguments.add_argument('--output', type=Path)
-arguments.add_argument('--feedback-metric', action='append', default=[])
+arguments.add_argument('--query', type=Path)
 arguments.add_argument('--ngrams', type=int)
-arguments.add_argument('--workers', type=int, default=mp.cpu_count())
+arguments.add_argument('--count', type=int, default=1000)
 args = arguments.parse_args()
 
 log = pz.util.get_logger(True)
+log.info('{0}'.format(args.query.stem))
 
-queue = mp.JoinableQueue()
-initargs = [
-    queue,
-    args.index,
-    args.feedback_metric,
-    args.indri,
-]
+relevance = pz.QueryRelevance(args.qrels)
+search = pz.IndriSearch(args.index, relevance, args.indri)
+writer = None
 
-log.info('++ begin {0}'.format(args.queries))
-with mp.Pool(args.workers, func, initargs) as pool:
-    for i in args.queries.iterdir():
-        out = args.output.joinpath(i.name)
-        if not out.exists():
-            components = pz.TrecDocument.components(i)
-            qrels = args.qrels.joinpath(str(components.topic))
-            model = i.suffix[1:] # without the '.'
+entry = {
+    'model': args.model,
+    'query': args.query.stem,
+    'ngrams': args.ngrams,
+}
 
-            info = Info(model, i.stem, args.ngrams)
-
-            queue.put((i, out, qrels, info))
-    queue.join()
-log.info('-- end')
+for i in filter(None, search.do(args.query)):
+    entry.update(**i.results)
+    if writer is None:
+        writer = csv.DictWriter(sys.stdout, fieldnames=entry.keys())
+        writer.writeheader()
+    writer.writerow(entry)
+sys.stdout.flush()
