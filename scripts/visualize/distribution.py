f8e7b04e623ef9bdf60d158bebe7208f1240e0ad
Jerome White <jerome@thewex.net>
2018-04-08 08:41:56 +0400

    New parameters from command line.
    Ability to specify y-axis log scaling and figure title.

--- scripts/visualize/distribution.py
+++ scripts/visualize/distribution.py
@@ -26,23 +26,28 @@ arguments = ArgumentParser()
 arguments.add_argument('--terms', type=Path)
 arguments.add_argument('--output', type=Path)
 arguments.add_argument('--measurement')
+arguments.add_argument('--title')
 arguments.add_argument('--x-min', type=float, default=0)
 arguments.add_argument('--x-max', type=float)
 arguments.add_argument('--normalize', action='store_true')
+arguments.add_argument('--log-scale', action='store_true')
+arguments.add_argument('--ngram', action='store_append')
 args = arguments.parse_args()
 
 df = pd.concat(aquire(args.measurement, args.terms))
 assert(not df.empty)
+if args.ngram:
+    df = df[df['ngram'].isin(args.ngram)]
 
 df = df.pivot(index='count', columns='version', values=args.measurement)
 if args.normalize:
     df /= df.sum()
 
-title = '../{0} - {1}'.format(args.terms.parts[-2:], args.measurement)
 df.plot.line(grid=True,
              xlim=(args.x_min, args.x_max),
-             ylim=(0, None),
-             title=title)
+             ylim=(0.0, None),
+             logy=args.log_scale,
+             title=args.title)
 
 img = args.output.joinpath(args.measurement).with_suffix('.png')
 plt.savefig(str(img), bbox_inches='tight')
